87c9a7fe3766128e977654ca759c228a7e3dd0f8
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2021-02-24 12:45:28 +0100

    Eksluderer handlebars.java fra wiremock deps

--- dkif/build.gradle.kts
+++ dkif/build.gradle.kts
@@ -2,5 +2,6 @@ dependencies {
     testImplementation("io.mockk:mockk:1.10.0")
     testImplementation("com.github.tomakehurst:wiremock:2.27.1") {
         exclude(group = "junit")
+        exclude("com.github.jknack.handlebars.java")
     }
 }
