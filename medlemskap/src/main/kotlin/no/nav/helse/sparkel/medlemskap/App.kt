e227a3b7ee5fe16bd26b12967dfbb201b4744c24
David Steinsland <david@steinsland.email>
2021-03-24 18:39:40 +0100

    bytter til ny ad-app i prod

--- medlemskap/src/main/kotlin/no/nav/helse/sparkel/medlemskap/App.kt
+++ medlemskap/src/main/kotlin/no/nav/helse/sparkel/medlemskap/App.kt
@@ -10,16 +10,11 @@ fun main() {
     RapidApplication.create(env).apply {
         val client = MedlemskapClient("http://medlemskap-oppslag.medlemskap.svc.nais.local", AzureClient(
             tokenEndpoint = env.getValue("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"),
-            clientId = "/var/run/secrets/nais.io/azuread/client_id".readFile() ?: env.getValue("AZURE_APP_CLIENT_ID"),
-            clientSecret = "/var/run/secrets/nais.io/azuread/client_secret".readFile() ?: env.getValue("AZURE_APP_CLIENT_SECRET")
+            clientId = env.getValue("AZURE_APP_CLIENT_ID"),
+            clientSecret = env.getValue("AZURE_APP_CLIENT_SECRET")
         ), env.getValue("MEDLEMSKAP_SCOPE"))
         Medlemskap(this, client)
     }.start()
 }
 
-private fun String.readFile() = try {
-    Files.readString(Paths.get(this))
-} catch (err: NoSuchFileException) {
-    null
-}
 
