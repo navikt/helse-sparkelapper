95d7a2dbf916cbcec8b559ae83ffee412363029b
Anders Østby <anders.ostby@nav.no>
2021-04-29 09:22:15 +0200

    Tester bruk av arrayopprettelse i oracle igjen

--- sykepengeperioder/src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/dbting/UtbetalingDAO.kt
+++ sykepengeperioder/src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/dbting/UtbetalingDAO.kt
@@ -5,14 +5,74 @@ import kotliquery.sessionOf
 import no.nav.helse.sparkel.sykepengeperioder.Fnr
 import no.nav.helse.sparkel.sykepengeperioder.Utbetalingshistorikk
 import no.nav.helse.sparkel.sykepengeperioder.Utbetalingsperiode
+import oracle.jdbc.OracleConnection
 import org.intellij.lang.annotations.Language
+import org.slf4j.LoggerFactory
 import java.time.LocalDate
 import javax.sql.DataSource
 
 class UtbetalingDAO(
     private val dataSource: DataSource
 ) {
-    internal fun utbetalinger(fnr: Fnr, vararg seq: Int): List<UtbetalingDTO> {
+    private companion object {
+        private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
+    }
+
+    internal fun utbetalinger(fnr: Fnr, vararg seq: Int) =
+        try {
+            utbetalingerSomErUsikker(fnr, *seq)
+        } catch (e: Throwable) {
+            sikkerlogg.info("Feilet ved bruk av den usikre array-opprettelsen. Bruker den som funker", e)
+            utbetalingerSomFunker(fnr, *seq)
+        }
+
+    private fun utbetalingerSomErUsikker(fnr: Fnr, vararg seq: Int): List<UtbetalingDTO> {
+        if (seq.isEmpty()) return emptyList()
+        return sessionOf(dataSource).use { session ->
+            @Language("Oracle")
+            val statement = """
+                    select
+        is15_utbetfom
+        , is15_utbettom
+        , is15_grad
+        , is15_op
+        , is15_utbetdato
+        , is15_dsats
+        , is15_type
+        , is15_arbgivnr
+        , is10_arbufoer_seq
+        from is_utbetaling_15
+        where f_nr = ?               -- 1
+        and is10_arbufoer_seq = ANY(?)  -- 2
+        and is15_korr <> 'KORR'
+                """
+            session.run(
+                if (session.connection.underlying.isWrapperFor(OracleConnection::class.java)) {
+                    val oracleConnection: OracleConnection =
+                        session.connection.underlying.unwrap(OracleConnection::class.java)
+                    val array = oracleConnection.createOracleArray("NUMBER", seq.toTypedArray())
+                    queryOf(statement, fnr.formatAsITFnr(), array)
+                } else {
+                    sikkerlogg.info("Underlying connection wrapper ikke OracleConnection")
+                    queryOf(statement, fnr.formatAsITFnr(), session.createArrayOf("NUMBER", seq.toList()))
+                }.map { rs ->
+                    UtbetalingDTO(
+                        fom = rs.intOrNullToLocalDate("is15_utbetfom"),
+                        tom = rs.intOrNullToLocalDate("is15_utbettom"),
+                        grad = rs.string("is15_grad").trim(),
+                        oppgjorType = rs.string("is15_op").trim { it <= '\u0020' },
+                        utbetalt = rs.intOrNullToLocalDate("is15_utbetdato"),
+                        dagsats = rs.double("is15_dsats"),
+                        periodeType = rs.string("is15_type").trim(),
+                        arbOrgnr = rs.string("is15_arbgivnr"),
+                        sekvensId = rs.int("is10_arbufoer_seq")
+                    )
+                }.asList
+            )
+        }
+    }
+
+    private fun utbetalingerSomFunker(fnr: Fnr, vararg seq: Int): List<UtbetalingDTO> {
         if (seq.isEmpty()) return emptyList()
         return sessionOf(dataSource).use { session ->
             val antallSpørsmålstegn = seq.joinToString(",") { "?" }
