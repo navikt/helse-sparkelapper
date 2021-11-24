922c7f6e44fb3ee071e795d04a8db24989b759fc
Simen Ullern <simen.ullern@nav.no>
2021-11-24 12:54:25 +0100

    Skriver om deseralisering av personhendelsedokument fra leesah
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>

--- personinfo/src/test/kotlin/no/nav/helse/sparkel/personinfo/leesah/PersonhendelseAvroDeserializerTest.kt
+++ personinfo/src/test/kotlin/no/nav/helse/sparkel/personinfo/leesah/PersonhendelseAvroDeserializerTest.kt
@@ -4,6 +4,7 @@ import org.apache.avro.generic.GenericData
 import org.apache.avro.generic.GenericDatumWriter
 import org.apache.avro.generic.GenericRecord
 import org.apache.avro.io.EncoderFactory
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.ByteArrayOutputStream
 import java.util.*
@@ -11,30 +12,30 @@ import java.util.*
 class PersonhendelseAvroDeserializerTest {
     val encoderFactory = EncoderFactory.get()
     @Test
-    fun `klarer å parse et pdl-dokument`() {
-        val bytes = nyttDokument("20046913337")
-        println(PersonhendelseAvroDeserializer().deserialize("leesah", bytes))
+    fun `klarer å parse personhendelsedokument fra leesah`() {
+        val dokument = nyttDokument("20046913337")
+        assertEquals(dokument, PersonhendelseAvroDeserializer().deserialize("leesah", serialize(dokument)))
     }
 
     fun nyttDokument(
         fodselsnummer: String,
         gradering: PersonhendelseOversetter.Gradering = PersonhendelseOversetter.Gradering.UGRADERT,
-    ): ByteArray {
-        val addressebeskyttelseSchema = PersonhendelseAvroDeserializer.schema.getField("adressebeskyttelse").schema().types.last()
+    ): GenericRecord = GenericData.Record(PersonhendelseAvroDeserializer.schema).apply {
+        val addressebeskyttelseSchema =
+            PersonhendelseAvroDeserializer.schema.getField("adressebeskyttelse").schema().types.last()
+        put("opplysningstype", "ADRESSEBESKYTTELSE_V1")
+        put("hendelseId", UUID.randomUUID().toString())
+        put("personidenter", listOf(fodselsnummer))
+        put("master", "skatt")
+        put("opprettet", 420L)
+        put("endringstype", GenericData.EnumSymbol(PersonhendelseAvroDeserializer.schema, "KORRIGERT"))
+        put("adressebeskyttelse", GenericData.Record(addressebeskyttelseSchema).apply {
+            put("gradering", GenericData.EnumSymbol(addressebeskyttelseSchema, gradering.name))
+        })
+    }
 
-        val writer = GenericDatumWriter<GenericRecord>(PersonhendelseAvroDeserializer.schema)
-        val record = GenericData.Record(PersonhendelseAvroDeserializer.schema).apply {
-            put("opplysningstype", "ADRESSEBESKYTTELSE_V1")
-            put("hendelseId", UUID.randomUUID().toString())
-            put("personidenter", listOf(fodselsnummer))
-            put("master", "skatt")
-            put("opprettet", 420L)
-            put("endringstype", GenericData.EnumSymbol(PersonhendelseAvroDeserializer.schema, "KORRIGERT"))
-            put("adressebeskyttelse", GenericData.Record(addressebeskyttelseSchema).apply {
-                put("gradering", GenericData.EnumSymbol(addressebeskyttelseSchema, gradering.name))
-            })
-        }
-
+    private fun serialize(record: GenericRecord): ByteArray {
+        val writer = GenericDatumWriter<GenericRecord>(record.schema)
         val bytesOut = ByteArrayOutputStream()
         val encoder = encoderFactory.binaryEncoder(bytesOut, null)
         writer.write(record, encoder)
