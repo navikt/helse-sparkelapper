159e5e321089ca957b104457cd3dc1b867e0d80b
Knut Nygaard <knutnyg@gmail.com>
2021-12-17 13:04:59 +0100

    River for å svare ut behov for oppslag på vergemål og fullmakter

---
+++ personinfo/src/main/kotlin/no/nav/helse/sparkel/personinfo/Vergemålløser.kt
@@ -0,0 +1,112 @@
+package no.nav.helse.sparkel.personinfo
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import net.logstash.logback.argument.StructuredArguments.keyValue
+import no.nav.helse.rapids_rivers.*
+import org.slf4j.LoggerFactory
+import java.time.LocalDate
+
+internal class Vergemålløser(
+    rapidsConnection: RapidsConnection,
+    private val personinfoService: PersoninfoService
+) : River.PacketListener {
+
+    private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
+
+    companion object {
+        const val behov = "Vergemål"
+        val objectMapper = jacksonObjectMapper()
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .registerModule(JavaTimeModule())
+    }
+
+    init {
+        River(rapidsConnection).apply {
+            validate { it.demandAll("@behov", listOf(behov)) }
+            validate { it.rejectKey("@løsning") }
+            validate { it.requireKey("@id") }
+            validate { it.requireKey("fødselsnummer") }
+        }.register(this)
+    }
+
+    override fun onError(problems: MessageProblems, context: MessageContext) {
+        sikkerlogg.error("forstod ikke behov $behov med melding\n${problems.toExtendedReport()}")
+    }
+
+    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        sikkerlogg.info("mottok melding: ${packet.toJson()}")
+        val behovId = packet["@id"].asText()
+        val fødselsnummer = packet["fødselsnummer"].asText()
+        try {
+            personinfoService.løsningForVergemål(behovId, fødselsnummer).let {
+                packet["@løsning"] = mapOf(behov to it.tilJson())
+            }
+            packet.toJson().let { løsningJson ->
+                context.publish(løsningJson)
+                sikkerlogg.info(
+                    "sender svar {}:\n\tløsning=$løsningJson",
+                    keyValue("id", behovId),
+                )
+            }
+        } catch (e: Exception) {
+            sikkerlogg.error("Feil under løsing av vergemål-behov for {}: ${e.message}", keyValue("fnr", fødselsnummer))
+        }
+    }
+
+    enum class VergemålType {
+        ensligMindreaarigAsylsoeker,
+        ensligMindreaarigFlyktning,
+        voksen,
+        midlertidigForVoksen,
+        mindreaarig,
+        midlertidigForMindreaarig,
+        forvaltningUtenforVergemaal,
+        stadfestetFremtidsfullmakt;
+
+        companion object {
+            private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
+            fun gyldig(type: String): Boolean {
+                return enumValues<VergemålType>().any { it.name == type }.also {
+                    sikkerlogg.warn("Fant ukjent vergemåltype:")
+                }
+            }
+        }
+    }
+
+
+    internal data class Vergemål(
+        val type: VergemålType
+    )
+
+    internal data class Fullmakt(
+        val områder: List<Område>,
+        val gyldigFraOgMed: LocalDate,
+        val gyldigTilOgMed: LocalDate
+    )
+
+    internal data class Resultat(
+        val vergemål: List<Vergemål>,
+        val fremtidsfullmakter: List<Vergemål>,
+        val fullmakter: List<Fullmakt>
+    ) {
+        fun tilJson(): JsonNode = objectMapper.valueToTree(this)
+    }
+
+    enum class Område {
+        Alle, Syk, Sym, ANNET;
+
+        companion object {
+            fun fra(verdi: String): Område {
+                return when (verdi) {
+                    "*" -> Alle
+                    "SYK" -> Syk
+                    "SYM" -> Sym
+                    else -> ANNET
+                }
+            }
+        }
+    }
+}
