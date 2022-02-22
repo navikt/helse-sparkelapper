8f5f8705e59e2fc8735029d6274029371ea78472
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-02-22 14:45:29 +0100

    Eksplisitt testing av null-verdier og manglende felter
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- gosys/src/test/kotlin/no/nav/helse/sparkel/gosys/OppgaveServiceTest.kt
+++ gosys/src/test/kotlin/no/nav/helse/sparkel/gosys/OppgaveServiceTest.kt
@@ -8,33 +8,27 @@ class OppgaveServiceTest {
 
     @Test
     fun `kan hente oppgaver`() {
-        val forventetAntall = 3
+        val forventetAntall = 2
 
+        val oppgaverSomSkalTellesMed = listOf(
+            objectNode().run {
+                put("behandlingstema", "ab0455") // Overgangssak fra Speil (skal gi varsel - ikke i enum)
+            },
+            objectNode().run {
+                putNull("behandlingstema")
+                putNull("behandlingstype")
+            }
+        )
+        val oppgaverSomIkkeSkalTellesMed = listOf(
+            objectNode().run {
+                put("behandlingstype", "ae0046") // Anke
+            },
+        )
         val oppgavehenter = Oppgavehenter { _, _ ->
-            jacksonObjectMapper().createObjectNode().run {
+            objectNode().run {
                 set("oppgaver", jacksonObjectMapper().createArrayNode().run {
-                    add(jacksonObjectMapper().createObjectNode().run {
-                        putNull("behandlingstema")
-                        put("behandlingstype", "ae0046") // Anke
-                    })
-                    add(jacksonObjectMapper().createObjectNode().run {
-                        put("behandlingstype", "ae0046") //
-                    })
-                    add(jacksonObjectMapper().createObjectNode().run {
-                        put("behandlingstema", "ab0455") // Overgangssak fra Speil (skal gi varsel - ikke i enum)
-                        putNull("behandlingstype")
-                    })
-                    add(jacksonObjectMapper().createObjectNode().run {
-                        put("behandlingstema", "ab0455") // Overgangssak fra Speil (skal gi varsel - ikke i enum)
-                    })
-                    add(jacksonObjectMapper().createObjectNode().run {
-                        putNull("behandlingstema")
-                        putNull("behandlingstype")
-                    })
-                    add(jacksonObjectMapper().createObjectNode().run {
-                        put("behandlingstema", "ab0338") // KLAGE_UNNTAK_FRA_ARBEIDSGIVERPERIODE
-                        put("behandlingstype", "ae0058")
-                    })
+                    addAll(oppgaverSomSkalTellesMed)
+                    addAll(oppgaverSomIkkeSkalTellesMed)
                 })
             }
         }
@@ -45,5 +39,72 @@ class OppgaveServiceTest {
 
         assertEquals(forventetAntall, svar)
     }
+
+    @Test
+    fun `håndterer manglende type eller tema som null når oppgaven ikke skal telles med`() {
+        val forventetAntall = 0
+        val oppgaverSomIkkeSkalTellesMed = listOf(
+            objectNode().run {
+                putNull("behandlingstema")
+                put("behandlingstype", "ae0046")
+            },
+            objectNode().run {
+                put("behandlingstype", "ae0046")
+            },
+            objectNode().run {
+                put("behandlingstema", "ab0200")
+            }
+        )
+        val oppgavehenter = Oppgavehenter { _, _ ->
+            objectNode().run {
+                set("oppgaver", jacksonObjectMapper().createArrayNode().run {
+                    addAll(oppgaverSomIkkeSkalTellesMed)
+                })
+            }
+        }
+
+        val service = OppgaveService(oppgavehenter)
+
+        val svar = service.løsningForBehov("behovId", "aktørId")
+
+        assertEquals(forventetAntall, svar)
+    }
+
+    @Test
+    fun `håndterer manglende type eller tema som null også når oppgaven skal telles med`() {
+        val forventetAntall = 5
+        val oppgaverSomSkalTellesMed = listOf(
+            objectNode().run {
+                put("behandlingstema", "ab0455")
+                putNull("behandlingstype")
+            },
+            objectNode().run {
+                put("behandlingstema", "ab0455")
+            },
+            objectNode().run {
+                put("behandlingstype", "ab0333")
+            },
+            objectNode().run {
+                putNull("behandlingstema")
+                putNull("behandlingstype")
+            },
+            objectNode()
+        )
+        val oppgavehenter = Oppgavehenter { _, _ ->
+            objectNode().run {
+                set("oppgaver", jacksonObjectMapper().createArrayNode().run {
+                    addAll(oppgaverSomSkalTellesMed)
+                })
+            }
+        }
+
+        val service = OppgaveService(oppgavehenter)
+
+        val svar = service.løsningForBehov("behovId", "aktørId")
+
+        assertEquals(forventetAntall, svar)
+    }
+
+    private fun objectNode() = jacksonObjectMapper().createObjectNode()
 }
 
