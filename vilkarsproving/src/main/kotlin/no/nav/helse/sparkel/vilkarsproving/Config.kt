ee872fd2953988143d3cf7ff1763b8dd7ad307e6
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-02-24 13:34:19 +0100

    Removed all trace of old impl
    Co-authored-by: Hermine Taule Kartveit :cat:
    Co-authored-by: Sindre Broch <sindre.broch@nav.no>

--- vilkarsproving/src/main/kotlin/no/nav/helse/sparkel/vilkarsproving/Config.kt
+++ vilkarsproving/src/main/kotlin/no/nav/helse/sparkel/vilkarsproving/Config.kt
@@ -1,24 +1,11 @@
 package no.nav.helse.sparkel.vilkarsproving
 
 import java.net.URL
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
 
-const val vaultServiceUserBase = "/var/run/secrets/nais.io/service_user"
-val vaultServiceUserBasePath: Path = Paths.get(vaultServiceUserBase)
-
-fun readServiceUserCredentials() = ServiceUser(
-    username = Files.readString(vaultServiceUserBasePath.resolve("username")),
-    password = Files.readString(vaultServiceUserBasePath.resolve("password"))
-)
 
 fun setUpEnvironment() =
     Environment(
         raw = System.getenv(),
-        stsSoapBaseUrl = System.getenv("STS_URL"),
-        egenAnsattBaseUrl = System.getenv("EGENANSATT_URL")
-            ?: error("Mangler env var EGENANSATT_URL"),
         skjermedeBaseURL = System.getenv("SKJERMEDE_BASE_URL")?.let {URL(it)}
             ?: error("Mangler env var SKJERMEDE_BASE_URL"),
         skjermendeOauthScope = System.getenv("SKJERMEDE_OAUTH_SCOPE")
@@ -33,17 +20,9 @@ fun setUpEnvironment() =
 
 data class Environment(
     val raw: Map<String, String>,
-    val stsBaseUrl: String = "http://security-token-service.default.svc.nais.local",
-    val stsSoapBaseUrl: String,
-    val egenAnsattBaseUrl: String,
     val skjermedeBaseURL: URL,
     val skjermendeOauthScope: String,
     val tokenEndpointURL: URL,
     val clientId: String,
     val clientSecret: String
-)
-
-data class ServiceUser(
-    val username: String,
-    val password: String
 )
