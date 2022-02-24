a3509a8109a24f8c85a7c50bf7baf15fc3660ac3
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-02-24 13:34:19 +0100

    Removed all trace of old impl
    Co-authored-by: Hermine Taule Kartveit :cat:
    Co-authored-by: Sindre Broch <sindre.broch@nav.no>

--- vilkarsproving/build.gradle.kts
+++ vilkarsproving/build.gradle.kts
@@ -6,11 +6,4 @@ dependencies {
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
 
-    implementation("com.sun.xml.ws:jaxws-ri:2.3.3")
-    implementation("org.apache.cxf:cxf-rt-frontend-jaxws:$cxfVersion")
-    implementation("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
-    implementation("org.apache.cxf:cxf-rt-transports-http:$cxfVersion")
-    implementation("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
-    implementation("javax.activation:activation:1.1.1")
-
 }
