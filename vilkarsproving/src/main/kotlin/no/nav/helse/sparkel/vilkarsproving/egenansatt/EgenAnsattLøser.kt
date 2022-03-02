4ed1a25f81ce5a7c4d66e0810fa9a07529c5e6a3
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-03-02 19:51:34 +0100

    Flytter mesteparten av koden ut av try-blokka

--- vilkarsproving/src/main/kotlin/no/nav/helse/sparkel/vilkarsproving/egenansatt/EgenAnsattLøser.kt
+++ vilkarsproving/src/main/kotlin/no/nav/helse/sparkel/vilkarsproving/egenansatt/EgenAnsattLøser.kt
@@ -27,35 +27,43 @@ internal class EgenAnsattLøser(
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        try {
-            sikkerlogg.info("mottok melding: ${packet.toJson()}")
-            val personErSkjermet = skjermedePersoner.erSkjermetPerson(packet["fødselsnummer"].asText(), packet["@id"].asText())
-            packet.setLøsning(behov, personErSkjermet)
-            if (personErSkjermet) sikkerlogg.info("Er egenAnsatt, fnr ${packet["fødselsnummer"].asText()}")
-            else sikkerlogg.info("Er ikke egenAnsatt, fnr ${packet["fødselsnummer"].asText()}")
+        sikkerlogg.info("mottok melding: ${packet.toJson()}")
 
-            log.info(
-                "løser behov {}",
-                keyValue("id", packet["@id"].asText())
-            )
-            sikkerlogg.info(
-                "løser behov {}",
-                keyValue("id", packet["@id"].asText())
-            )
-
-            context.publish(packet.toJson())
+        val personErSkjermet = try {
+            skjermedePersoner.erSkjermetPerson(packet["fødselsnummer"].asText(), packet["@id"].asText())
         } catch (err: Exception) {
-            log.error(
-                "feil ved henting av egen ansatt: ${err.message} for behov {}",
-                keyValue("id", packet["@id"].asText()),
-                err
-            )
-            sikkerlogg.error(
-                "feil ved henting av egen ansatt: ${err.message} for behov {}",
-                keyValue("id", packet["@id"].asText()),
-                err
-            )
+            loggFeil(err, packet)
+            return
         }
+
+        packet.setLøsning(behov, personErSkjermet)
+
+        if (personErSkjermet) sikkerlogg.info("Er egenAnsatt, fnr ${packet["fødselsnummer"].asText()}")
+        else sikkerlogg.info("Er ikke egenAnsatt, fnr ${packet["fødselsnummer"].asText()}")
+
+        log.info(
+            "løser behov {}",
+            keyValue("id", packet["@id"].asText())
+        )
+        sikkerlogg.info(
+            "løser behov {}",
+            keyValue("id", packet["@id"].asText())
+        )
+
+        context.publish(packet.toJson())
+    }
+
+    private fun loggFeil(err: Exception, packet: JsonMessage) {
+        log.error(
+            "feil ved henting av egen ansatt: ${err.message} for behov {}",
+            keyValue("id", packet["@id"].asText()),
+            err
+        )
+        sikkerlogg.error(
+            "feil ved henting av egen ansatt: ${err.message} for behov {}",
+            keyValue("id", packet["@id"].asText()),
+            err
+        )
     }
 
     override fun onError(problems: MessageProblems, context: MessageContext) {}
