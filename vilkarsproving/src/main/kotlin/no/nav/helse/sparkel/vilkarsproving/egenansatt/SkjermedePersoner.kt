1543f27cd63057b21c672746a5d81f08cc34eebd
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-03-14 13:55:10 +0100

    Fjerner sts policies er ikkje lenger i bruk Logger nå ut før og etter call til erSkjermetPerson, får å se tidsbruker Setter også timeouten opp til 60 sekunder for å se om det hjelper

--- vilkarsproving/src/main/kotlin/no/nav/helse/sparkel/vilkarsproving/egenansatt/SkjermedePersoner.kt
+++ vilkarsproving/src/main/kotlin/no/nav/helse/sparkel/vilkarsproving/egenansatt/SkjermedePersoner.kt
@@ -7,6 +7,8 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.time.Duration
+import java.time.LocalDateTime
+import org.slf4j.LoggerFactory
 
 class SkjermedePersoner(
     private val tokenSupplier: () -> String,
@@ -16,6 +18,7 @@ class SkjermedePersoner(
 
     private companion object {
         private val objectMapper = jacksonObjectMapper()
+        private val log = LoggerFactory.getLogger(this::class.java)
     }
 
     internal fun erSkjermetPerson(fødselsnummer: String, behovId: String): Boolean {
@@ -30,12 +33,14 @@ class SkjermedePersoner(
             .header("Accept", "application/json")
             .header("Nav-Call-Id", behovId)
             .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-            .timeout(Duration.ofSeconds(30))
+            .timeout(Duration.ofSeconds(60))
             .build()
 
         val responseHandler = HttpResponse.BodyHandlers.ofString()
 
+        log.info("Time before sending http request: " + LocalDateTime.now() )
         val response = httpClient.send(request, responseHandler)
+        log.info("Time after sending http request: " + LocalDateTime.now() )
 
         if (response.statusCode() != 200) {
             throw RuntimeException("error (responseCode=${response.statusCode()}) from Skjerming")
