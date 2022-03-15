94ad8c0004e7cdce4e295d9f8c41d45a1439ad7a
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-03-15 08:07:48 +0100

    Går over til å benytte ktor client for http kall

--- vilkarsproving/src/main/kotlin/no/nav/helse/sparkel/vilkarsproving/egenansatt/SkjermedePersoner.kt
+++ vilkarsproving/src/main/kotlin/no/nav/helse/sparkel/vilkarsproving/egenansatt/SkjermedePersoner.kt
@@ -1,17 +1,27 @@
 package no.nav.helse.sparkel.vilkarsproving.egenansatt
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.ktor.client.call.receive
+import io.ktor.client.request.accept
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.statement.HttpStatement
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.client.HttpClient as KtorHttpClient
 import java.net.URI
 import java.net.URL
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.time.Duration
+import kotlinx.coroutines.runBlocking
 
 class SkjermedePersoner(
     private val tokenSupplier: () -> String,
     private val baseUrl: URL,
-    private val httpClient: HttpClient = HttpClient.newHttpClient()
+    private val httpClient: HttpClient = HttpClient.newHttpClient(),
+    private val ktorHttpClient: KtorHttpClient
 ) {
 
     private companion object {
@@ -23,6 +33,31 @@ class SkjermedePersoner(
         val requestBody = objectMapper.writeValueAsString(
             SkjermetDataRequestDTO(fødselsnummer)
         )
+
+        try {
+            runBlocking {
+                retry("er_skjermet_person") {
+                    val httpResponse = ktorHttpClient.post<HttpStatement>("$baseUrl/skjermet") {
+                        header("Authorization", "Bearer ${tokenSupplier()}")
+                        header("Nav-Call-Id", behovId)
+                        accept(ContentType.Application.Json)
+                        contentType(ContentType.Application.Json)
+                        body = requestBody
+                    }.execute()
+                    when (httpResponse.status.value) {
+                        200 -> {
+                            log.info("ktorHttpClient kallet fungerte fikk dette som svar: " + httpResponse.call.response.receive())
+                        }
+                        else -> {
+                            log.warn("Statuskode: ${httpResponse.status.description} feil på oppslag mot skjermet")
+                        }
+                    }
+                }
+            }
+        }
+        catch (exception: Exception) {
+            log.warn("ktorHttpClient feilet på oppslag mot skjermet" , exception)
+        }
 
         val request = HttpRequest.newBuilder(URI.create("$baseUrl/skjermet"))
             .header("Authorization", "Bearer ${tokenSupplier()}")
