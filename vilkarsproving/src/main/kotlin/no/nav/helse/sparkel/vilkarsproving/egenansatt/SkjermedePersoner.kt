6d2e435c432029d0f6867c3b14f4e5c0cafeb5ee
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-03-17 08:37:24 +0100

    Benytter nå ktor client som http klient for å kalle skjermet
    Fjerner bruk av java.net.http.HttpClient da dette var den eneste som benyttet denne http klienten
    
    Co-authored-by: David Steinsland <david@davidsteinsland.net>
    Co-authored-by: Nina Bakken <nina.bakken@nav.no>

--- vilkarsproving/src/main/kotlin/no/nav/helse/sparkel/vilkarsproving/egenansatt/SkjermedePersoner.kt
+++ vilkarsproving/src/main/kotlin/no/nav/helse/sparkel/vilkarsproving/egenansatt/SkjermedePersoner.kt
@@ -1,7 +1,6 @@
 package no.nav.helse.sparkel.vilkarsproving.egenansatt
 
 import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.call.receive
 import io.ktor.client.features.HttpTimeout
@@ -13,21 +12,15 @@ import io.ktor.client.request.post
 import io.ktor.client.statement.HttpStatement
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
-import io.ktor.client.HttpClient as KtorHttpClient
-import java.net.URI
+import io.ktor.client.HttpClient
 import java.net.URL
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
 import java.time.Duration
 import kotlinx.coroutines.runBlocking
-import org.slf4j.LoggerFactory
 
 class SkjermedePersoner(
     private val tokenSupplier: () -> String,
     private val baseUrl: URL,
-    private val httpClient: HttpClient = HttpClient.newHttpClient(),
-    private val ktorHttpClient: KtorHttpClient = KtorHttpClient {
+    private val ktorHttpClient: HttpClient = HttpClient {
         install(JsonFeature)
         {
             serializer = JacksonSerializer {
@@ -36,72 +29,32 @@ class SkjermedePersoner(
             }
         }
         install(HttpTimeout) {
-            connectTimeoutMillis = 10000
-            requestTimeoutMillis = 10000
-            socketTimeoutMillis = 10000
+            connectTimeoutMillis = Duration.ofSeconds(60).toMillis()
+            requestTimeoutMillis = Duration.ofSeconds(60).toMillis()
+            socketTimeoutMillis = Duration.ofSeconds(60).toMillis()
         }
     }
 ) {
 
-    private companion object {
-        private val objectMapper = jacksonObjectMapper()
-    }
-
-    internal fun erSkjermetPerson(fødselsnummer: String, behovId: String): Boolean {
-
-        testerSkjermetPersonMedKtorKlient(behovId, fødselsnummer)
-
-        val requestBody = objectMapper.writeValueAsString(
-            SkjermetDataRequestDTO(fødselsnummer)
-        )
-
-        val request = HttpRequest.newBuilder(URI.create("$baseUrl/skjermet"))
-            .header("Authorization", "Bearer ${tokenSupplier()}")
-            .header("Content-Type", "application/json")
-            .header("Accept", "application/json")
-            .header("Nav-Call-Id", behovId)
-            .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-            .timeout(Duration.ofSeconds(60))
-            .build()
-
-        val responseHandler = HttpResponse.BodyHandlers.ofString()
-
-        val response = httpClient.send(request, responseHandler)
-
-        if (response.statusCode() != 200) {
-            throw RuntimeException("error (responseCode=${response.statusCode()}) from Skjerming")
-        }
-
-        return objectMapper.readTree(response.body()).asBoolean()
-    }
-
-    private fun testerSkjermetPersonMedKtorKlient(behovId: String, fødselsnummer: String) {
-        val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
-
-        try {
-            runBlocking {
-                val httpResponse = ktorHttpClient.post<HttpStatement>("$baseUrl/skjermet") {
-                    header("Authorization", "Bearer ${tokenSupplier()}")
-                    header("Nav-Call-Id", behovId)
-                    accept(ContentType.Application.Json)
-                    contentType(ContentType.Application.Json)
-                    body = SkjermetDataRequestDTO(fødselsnummer)
-                }.execute()
-                when (httpResponse.status.value) {
-                    200 -> {
-                        val response: Boolean = httpResponse.call.response.receive()
-                        sikkerlogg.info("ktorHttpClient kallet fungerte fikk dette som svar: " + response)
-                    }
-                    else -> {
-                        sikkerlogg.warn("Statuskode: ${httpResponse.status.description} feil på oppslag mot skjermet")
-                    }
+    internal fun erSkjermetPerson(fødselsnummer: String, behovId: String): Boolean =
+        runBlocking {
+            val httpResponse = ktorHttpClient.post<HttpStatement>("$baseUrl/skjermet") {
+                header("Authorization", "Bearer ${tokenSupplier()}")
+                header("Nav-Call-Id", behovId)
+                accept(ContentType.Application.Json)
+                contentType(ContentType.Application.Json)
+                body = SkjermetDataRequestDTO(fødselsnummer)
+            }.execute()
+            when (httpResponse.status.value) {
+                200 -> {
+                    val response = httpResponse.call.response.receive<Boolean>()
+                    return@runBlocking response
+                }
+                else -> {
+                    throw RuntimeException("error (responseCode=${httpResponse.status.value}) from Skjerming")
                 }
             }
-        } catch (exception: Exception) {
-            sikkerlogg.warn("ktorHttpClient feilet på oppslag mot skjermet", exception)
         }
-    }
-
 }
 
 private data class SkjermetDataRequestDTO(
