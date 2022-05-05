9939d004a04bd3d05ce73b4b610cdcbf77810b6b
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2022-05-05 15:16:00 +0200

    Logger ugyldige meldinger fra Leesah
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>

--- personinfo/src/main/kotlin/no/nav/helse/sparkel/personinfo/leesah/PersonhendelseAvroDeserializer.kt
+++ personinfo/src/main/kotlin/no/nav/helse/sparkel/personinfo/leesah/PersonhendelseAvroDeserializer.kt
@@ -5,22 +5,29 @@ import org.apache.avro.generic.GenericDatumReader
 import org.apache.avro.generic.GenericRecord
 import org.apache.avro.io.DecoderFactory
 import org.apache.kafka.common.serialization.Deserializer
+import org.slf4j.LoggerFactory
 
 class PersonhendelseAvroDeserializer : Deserializer<GenericRecord> {
     private val decoderFactory: DecoderFactory = DecoderFactory.get()
     override fun deserialize(topic: String, data: ByteArray): GenericRecord {
-        val reader = GenericDatumReader<GenericRecord>(schema)
-        val decoder = decoderFactory.binaryDecoder(data, null)
-        /*
-        KafkaAvroSerializer legger på 5 bytes, 1 magic byte og 4 som sier noe om hvilke entry i schema registeret som
-        brukes. Siden vi ikke ønsker å ha et dependency til schema registryet har vi en egen deserializer og skipper de
-        5 første bytene
-         */
-        decoder.skipFixed(5)
-        return reader.read(null, decoder)
+        try {
+            val reader = GenericDatumReader<GenericRecord>(schema)
+            val decoder = decoderFactory.binaryDecoder(data, null)
+            /*
+            KafkaAvroSerializer legger på 5 bytes, 1 magic byte og 4 som sier noe om hvilke entry i schema registeret som
+            brukes. Siden vi ikke ønsker å ha et dependency til schema registryet har vi en egen deserializer og skipper de
+            5 første bytene
+             */
+            decoder.skipFixed(5)
+            return reader.read(null, decoder)
+        } catch (throwable: Throwable) {
+            sikkerlogg.warn("Mottok ugyldig melding fra Leesah '${String(data)}'")
+            throw throwable
+        }
     }
 
     companion object {
+        private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
         val schema = Schema.Parser().parse(PersonhendelseAvroDeserializer::class.java.getResourceAsStream("/pdl/PersonhendelseProto.avsc"))
     }
 }
--- personinfo/src/test/kotlin/no/nav/helse/sparkel/personinfo/leesah/PersonhendelseAvroDeserializerTest.kt
+++ personinfo/src/test/kotlin/no/nav/helse/sparkel/personinfo/leesah/PersonhendelseAvroDeserializerTest.kt
@@ -1,19 +1,30 @@
 package no.nav.helse.sparkel.personinfo.leesah
 
+import java.io.EOFException
 import no.nav.helse.sparkel.personinfo.leesah.PersonhendelseFactory.nyttDokument
 import no.nav.helse.sparkel.personinfo.leesah.PersonhendelseFactory.serialize
-import org.apache.avro.generic.GenericDatumWriter
-import org.apache.avro.generic.GenericRecord
-import org.apache.avro.io.EncoderFactory
-import org.apache.commons.codec.binary.Base64
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.io.ByteArrayOutputStream
+import org.junit.jupiter.api.assertThrows
 
 class PersonhendelseAvroDeserializerTest {
     @Test
     fun `klarer å parse personhendelsedokument fra leesah`() {
         val dokument = nyttDokument("20046913337")
         assertEquals(dokument, PersonhendelseAvroDeserializer().deserialize("leesah", serialize(dokument)))
+    }
+
+    @Test
+    fun `tom melding`() {
+        assertThrows<EOFException> {
+            PersonhendelseAvroDeserializer().deserialize("leesah", ByteArray(0))
+        }
+    }
+
+    @Test
+    fun `ugyldig melding`() {
+        assertThrows<EOFException> {
+            PersonhendelseAvroDeserializer().deserialize("leesah", """{"test": true}""".toByteArray())
+        }
     }
 }
