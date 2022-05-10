02cb0ce4f594ce1bbeb14678bac01dda2e3ea121
Sindre Broch <sindre.broch@nav.no>
2022-05-10 10:39:18 +0200

    Legger til en enkel throttle på gosys_oppgave_endret
    * Unngå mange events for samme fnr som kommer inn rett etter hverandre
    * Slår til hvis fnr er samme som det som sist kom inn, og innenfor en ramme på 30 sekunder

--- oppgave-endret/src/main/kotlin/no/nav/helse/sparkel/oppgaveendret/GosysOppgaveEndretProducer.kt
+++ oppgave-endret/src/main/kotlin/no/nav/helse/sparkel/oppgaveendret/GosysOppgaveEndretProducer.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.sparkel.oppgaveendret
 
+import java.time.Duration
 import java.time.LocalDateTime
 import java.util.UUID
 import no.nav.helse.rapids_rivers.JsonMessage
@@ -14,19 +15,23 @@ class GosysOppgaveEndretProducer(
     private val logger = LoggerFactory.getLogger(this::class.java)
     private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
 
+    private var forrigeFnr: String? = null
+    private var forrigeOppdatering = LocalDateTime.MIN
+    private val throttleDuration = Duration.ofSeconds(30)
+
     private val GOSYS = "FS22"
 
     fun onPacket(oppgave: Oppgave) {
         if (oppgave.behandlesAvApplikasjon != GOSYS || oppgave.tema != "SYK") return
         logger.info("Mottok endring på gosysoppgave på tema SYK oppgaven " + oppgave.id)
         if (oppgave.ident == null) return
-
         if (!oppgave.ident.folkeregisterident.isNullOrEmpty() && oppgave.ident.identType == IdentType.AKTOERID) {
+            if (throttle(oppgave.ident.folkeregisterident)) return
             logger.info("Har folkeregisterident og aktorId på oppgaven " + oppgave.id)
             packetAndPublish(oppgave.ident.folkeregisterident, oppgave.ident.verdi)
         } else {
             sikkerlogg.info("Oppgave: " + oppgave)
-            logger.error("Mangler folkeregisterident og aktorId på oppgaven " + oppgave.id)
+            logger.error("Mangler folkeregisterident og/eller aktorId på oppgaven " + oppgave.id)
         }
     }
 
@@ -41,6 +46,13 @@ class GosysOppgaveEndretProducer(
             )
         )
         rapidsConnection.publish(fødselsnummer, packet.toJson())
+    }
+
+    private fun throttle(fnr: String): Boolean {
+        if (fnr == forrigeFnr && forrigeOppdatering.plusNanos(throttleDuration.toNanos()) > LocalDateTime.now()) return true
+        forrigeFnr = fnr
+        forrigeOppdatering = LocalDateTime.now()
+        return false
     }
 
 }
