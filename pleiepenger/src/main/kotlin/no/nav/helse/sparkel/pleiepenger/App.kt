e650467ce2dfc01bd420816174f126d13b5271df
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2022-05-23 11:19:33 +0200

    Klargjør for henting fra K9 - Skrur på henting fra Abakus i dev

--- pleiepenger/src/main/kotlin/no/nav/helse/sparkel/pleiepenger/App.kt
+++ pleiepenger/src/main/kotlin/no/nav/helse/sparkel/pleiepenger/App.kt
@@ -1,10 +1,15 @@
 package no.nav.helse.sparkel.pleiepenger
 
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Paths
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.sparkel.pleiepenger.infotrygd.AzureClient
 import no.nav.helse.sparkel.pleiepenger.infotrygd.InfotrygdClient
 import no.nav.helse.sparkel.pleiepenger.infotrygd.InfotrygdService
+import no.nav.helse.sparkel.pleiepenger.k9.AbakusClient
+import no.nav.helse.sparkel.pleiepenger.k9.ClientSecretBasic
 
 fun main() {
     val app = createApp(System.getenv())
@@ -22,11 +27,31 @@ internal fun createApp(env: Map<String, String>): RapidsConnection {
         accesstokenScope = env.getValue("INFOTRYGD_SCOPE"),
         azureClient = azureClient
     )
-    val infotrygdService = InfotrygdService(infotrygdClient)
 
-    return RapidApplication.create(env).apply {
-        Pleiepengerløser(this, infotrygdService)
-        Omsorgspengerløser(this, infotrygdService)
-        Opplæringspengerløser(this, infotrygdService)
+    return if (env.getValue("NAIS_CLUSTER_NAME") == "dev-fss") {
+        val stsClient = ClientSecretBasic(
+            tokenEndpoint = env.getValue("STS_TOKEN_ENDPOINT"),
+            clientId = Files.readString(Paths.get("/var/run/secrets/nais.io/service_user/username")),
+            clientSecret = Files.readString(Paths.get("/var/run/secrets/nais.io/service_user/password"))
+        )
+        val abakusClient = AbakusClient(
+            url = URL(env.getValue("ABAKUS_URL")),
+            accessTokenClient = stsClient
+        )
+        RapidApplication.create(env).apply {
+            PleiepengerløserV2(this, infotrygdClient, abakusClient)
+            OmsorgspengerløserV2(this, infotrygdClient, abakusClient)
+            PleiepengerløserV2(this, infotrygdClient, abakusClient)
+        }
+    } else {
+        val infotrygdService = InfotrygdService(
+            infotrygdClient = infotrygdClient
+        )
+
+        RapidApplication.create(env).apply {
+            Pleiepengerløser(this, infotrygdService)
+            Omsorgspengerløser(this, infotrygdService)
+            Opplæringspengerløser(this, infotrygdService)
+        }
     }
 }
