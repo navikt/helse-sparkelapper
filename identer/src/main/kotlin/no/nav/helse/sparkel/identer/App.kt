6386a6fbbaec0a752b29889f78f18bc25778cecb
Øyvind Hagen <oyvind.hagen@nav.no>
2022-06-14 16:35:04 +0200

    Legger på mer logging

--- identer/src/main/kotlin/no/nav/helse/sparkel/identer/App.kt
+++ identer/src/main/kotlin/no/nav/helse/sparkel/identer/App.kt
@@ -2,6 +2,7 @@ package no.nav.helse.sparkel.identer
 
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
+import org.slf4j.LoggerFactory
 
 val PDL_AKTØR_TOPIC = "aapen-person-pdl-aktor-v1"
 
@@ -11,10 +12,13 @@ fun main() {
 }
 
 internal fun createApp(env: Map<String, String>): RapidsConnection {
+    private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
     val kafkaConsumer = createConsumer()
     kafkaConsumer.subscribe(listOf(PDL_AKTØR_TOPIC))
 
     return RapidApplication.create(env).apply {
+        sikkerlogg.info("gcp-srvsparkelidenter-credentials=${System.getenv("gcp-srvsparkelidenter-credentials")}")
+        sikkerlogg.info("srvsparkelidenter-username=${System.getenv("srvsparkelidenter-username")}")
         val aktørConsumer = AktørConsumer(this, kafkaConsumer, IdenthendelseHandler())
         Thread(aktørConsumer).start()
         this.register(object : RapidsConnection.StatusListener {
