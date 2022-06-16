248b56b98e8fccf3ab784217857e8de8be090158
Øyvind Hagen <oyvind.hagen@nav.no>
2022-06-16 14:12:35 +0200

    Fjerner unødig logging

--- identer/src/main/kotlin/no/nav/helse/sparkel/identer/App.kt
+++ identer/src/main/kotlin/no/nav/helse/sparkel/identer/App.kt
@@ -2,7 +2,6 @@ package no.nav.helse.sparkel.identer
 
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
-import org.slf4j.LoggerFactory
 
 val PDL_AKTØR_TOPIC = "aapen-person-pdl-aktor-v1"
 
@@ -12,16 +11,10 @@ fun main() {
 }
 
 internal fun createApp(env: Map<String, String>): RapidsConnection {
-    val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
     val kafkaConsumer = createConsumer()
     kafkaConsumer.subscribe(listOf(PDL_AKTØR_TOPIC))
 
     return RapidApplication.create(env).apply {
-        sikkerlogg.info("env-vars:")
-        System.getenv().keys.forEach {
-            sikkerlogg.info(it)
-        }
-        sikkerlogg.info("srvsparkelidenter_username=${System.getenv("srvsparkelidenter_username")}")
         val aktørConsumer = AktørConsumer(this, kafkaConsumer, IdenthendelseHandler())
         Thread(aktørConsumer).start()
         this.register(object : RapidsConnection.StatusListener {
