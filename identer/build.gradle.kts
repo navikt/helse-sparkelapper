c4b84da5b740cd4a28cf2a57060890692fbad18e
Øyvind Hagen <oyvind.hagen@nav.no>
2022-06-17 12:45:16 +0200

    Legger til postgres sql-driver

--- identer/build.gradle.kts
+++ identer/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation("org.apache.avro:avro:1.11.0")
     implementation("org.flywaydb:flyway-core:8.5.13")
+    implementation("org.postgresql:postgresql:42.4.0")
     implementation("com.zaxxer:HikariCP:5.0.1")
 }
