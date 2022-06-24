da431449701e7800247c8d0dac1f0869e8267c72
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-06-24 14:12:45 +0200

    Flytter versjons nummer til egene variabler
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>
    Co-authored-by: Sindre Broch <sindre.broch@nav.no>
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- identer/build.gradle.kts
+++ identer/build.gradle.kts
@@ -1,9 +1,17 @@
-dependencies {
-    implementation("org.apache.avro:avro:1.11.0")
-    implementation("org.flywaydb:flyway-core:8.5.13")
-    implementation("com.github.seratch:kotliquery:1.8.0")
-    implementation("org.postgresql:postgresql:42.4.0")
-    implementation("com.zaxxer:HikariCP:5.0.1")
 
-    testImplementation("org.testcontainers:postgresql:1.17.2")
+val avroVersion = "1.11.0"
+val flyAwayCoreVersion = "8.5.13"
+val kotliqueryVersion = "1.8.0"
+val postgresqlVersion = "42.4.0"
+val hikariCPVersion = "5.0.1"
+val testcontainersOostgresqlVersion = "1.17.2"
+
+dependencies {
+    implementation("org.apache.avro:avro:$avroVersion")
+    implementation("org.flywaydb:flyway-core:$flyAwayCoreVersion")
+    implementation("com.github.seratch:kotliquery:$kotliqueryVersion")
+    implementation("org.postgresql:postgresql:$postgresqlVersion")
+    implementation("com.zaxxer:HikariCP:$hikariCPVersion")
+
+    testImplementation("org.testcontainers:postgresql:$testcontainersOostgresqlVersion")
 }
