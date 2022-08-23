bb526cf9f691c3d89463394634844285c151a5f9
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2022-08-23 16:29:10 +0200

    Revert "Bruke samme datasource for feriepenger som ellers :palm_tree:"
    This reverts commit 0874f87e093fa88bd8363d969743acce27665cd3.

--- sykepengeperioder/src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/App.kt
+++ sykepengeperioder/src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/App.kt
@@ -5,11 +5,11 @@ import com.zaxxer.hikari.HikariDataSource
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.sparkel.sykepengeperioder.DatabaseConfig.Companion.databaseConfig
+import no.nav.helse.sparkel.sykepengeperioder.DatabaseConfig.Companion.databaseConfigForFeriepenger
+import no.nav.helse.sparkel.sykepengeperioder.dbting.*
 import no.nav.helse.sparkel.sykepengeperioder.dbting.FeriepengeDAO
 import no.nav.helse.sparkel.sykepengeperioder.dbting.InntektDAO
 import no.nav.helse.sparkel.sykepengeperioder.dbting.PeriodeDAO
-import no.nav.helse.sparkel.sykepengeperioder.dbting.StatslønnDAO
-import no.nav.helse.sparkel.sykepengeperioder.dbting.UtbetalingDAO
 
 fun main() {
     val app = createApp(System.getenv())
@@ -26,12 +26,21 @@ internal fun createApp(env: Map<String, String>): RapidsConnection {
         schema = databaseConfig.schema
     })
 
+    val databaseConfigForFeriepenger = env.databaseConfigForFeriepenger()
+
+    val dataSourceForFeriepenger = HikariDataSource(HikariConfig().apply {
+        jdbcUrl = databaseConfigForFeriepenger.jdbcUrl
+        username = databaseConfigForFeriepenger.username
+        password = databaseConfigForFeriepenger.password
+        schema = databaseConfigForFeriepenger.schema
+    })
+
     val infotrygdService = InfotrygdService(
         PeriodeDAO(dataSource),
         UtbetalingDAO(dataSource),
         InntektDAO(dataSource),
         StatslønnDAO(dataSource),
-        FeriepengeDAO(dataSource)
+        FeriepengeDAO(dataSourceForFeriepenger)
     )
 
     return RapidApplication.create(env).apply {
