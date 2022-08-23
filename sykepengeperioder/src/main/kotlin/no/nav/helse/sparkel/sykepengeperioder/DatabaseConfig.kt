f682bbdd406efeab950c08535d5bcdd35d5acd32
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2022-08-23 16:34:14 +0200

    Bruker alltid sykepengers egen replika 🏮
    - For feriepenger brukes fortsatt felles replika
    
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- sykepengeperioder/src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/DatabaseConfig.kt
+++ sykepengeperioder/src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/DatabaseConfig.kt
@@ -12,29 +12,12 @@ internal class DatabaseConfig private constructor(
     internal companion object {
         private val logger = LoggerFactory.getLogger(DatabaseConfig::class.java)
 
-        internal fun Map<String, String>.databaseConfig(): DatabaseConfig {
-            val usernameFile = File("/var/run/secrets/nais.io/oracle/creds/username")
-            val passwordFile = File("/var/run/secrets/nais.io/oracle/creds/password")
-            val jdbcUrlFile = File("/var/run/secrets/nais.io/oracle/config/jdbc_url")
-            val schema = get("DATABASE_SCHEMA")
-            return if (usernameFile.exists() && passwordFile.exists() && jdbcUrlFile.exists() && schema != null) {
-                logger.info("Kobler opp mot sykepengers egen replika.")
-                DatabaseConfig(
-                    username = usernameFile.readText(),
-                    password = passwordFile.readText(),
-                    jdbcUrl = jdbcUrlFile.readText(),
-                    schema = schema
-                )
-            } else {
-                logger.info("Kobler opp mot felles replika.")
-                DatabaseConfig(
-                    username = getValue("INFOTRYGDSP_USERNAME"),
-                    password = getValue("INFOTRYGDSP_PASSWORD"),
-                    jdbcUrl = getValue("INFOTRYGDSP_URL"),
-                    schema = getValue("INFOTRYGDSP_SCHEMA")
-                )
-            }
-        }
+        internal fun Map<String, String>.databaseConfig() = DatabaseConfig(
+            username = File("/var/run/secrets/nais.io/oracle/creds/username").readText(),
+            password = File("/var/run/secrets/nais.io/oracle/creds/password").readText(),
+            jdbcUrl = File("/var/run/secrets/nais.io/oracle/config/jdbc_url").readText(),
+            schema = getValue("DATABASE_SCHEMA")
+        )
 
         internal fun Map<String, String>.databaseConfigForFeriepenger(): DatabaseConfig {
             logger.info("Kobler opp mot felles replika til bruk for beregning av feriepenger.")
