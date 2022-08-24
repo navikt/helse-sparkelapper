7e859bbef9a72fbc7968e51ef58141f2b00f8923
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-08-24 20:16:24 +0200

    Bruker sykepengers replika for feriepenger
    Co-authored-by: Erik M.Forsman <erik.maximilian.forsman@nav.no>

--- sykepengeperioder/src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/DatabaseConfig.kt
+++ sykepengeperioder/src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/DatabaseConfig.kt
@@ -1,7 +1,6 @@
 package no.nav.helse.sparkel.sykepengeperioder
 
 import java.io.File
-import org.slf4j.LoggerFactory
 
 internal class DatabaseConfig private constructor(
     val username: String,
@@ -10,23 +9,11 @@ internal class DatabaseConfig private constructor(
     val schema: String
 ) {
     internal companion object {
-        private val logger = LoggerFactory.getLogger(DatabaseConfig::class.java)
-
         internal fun Map<String, String>.databaseConfig() = DatabaseConfig(
             username = File("/var/run/secrets/nais.io/oracle/creds/username").readText(),
             password = File("/var/run/secrets/nais.io/oracle/creds/password").readText(),
             jdbcUrl = File("/var/run/secrets/nais.io/oracle/config/jdbc_url").readText(),
             schema = getValue("DATABASE_SCHEMA")
         )
-
-        internal fun Map<String, String>.databaseConfigForFeriepenger(): DatabaseConfig {
-            logger.info("Kobler opp mot felles replika til bruk for beregning av feriepenger.")
-            return DatabaseConfig(
-                username = getValue("INFOTRYGDSP_USERNAME"),
-                password = getValue("INFOTRYGDSP_PASSWORD"),
-                jdbcUrl = getValue("INFOTRYGDSP_URL"),
-                schema = getValue("INFOTRYGDSP_SCHEMA")
-            )
-        }
     }
 }
