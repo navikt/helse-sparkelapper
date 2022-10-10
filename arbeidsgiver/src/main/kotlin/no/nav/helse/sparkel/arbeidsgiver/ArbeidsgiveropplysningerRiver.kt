a766fc0ddbe298869b44fdea36833d08df12e2fe
Gustav Berggren <83211434+b162214@users.noreply.github.com>
2022-10-10 13:49:31 +0200

    Lag ny river for arbeidgiveropplysninger (#11)
    * Lag ny river for arbeidgiveropplysninger
    
    
    Co-authored-by: Mikael Bjerga <mikael.bjerga@nav.no>
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

---
+++ arbeidsgiver/src/main/kotlin/no/nav/helse/sparkel/arbeidsgiver/ArbeidsgiveropplysningerRiver.kt
@@ -0,0 +1,33 @@
+package no.nav.helse.sparkel.arbeidsgiver
+
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.MessageProblems
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.River
+import org.slf4j.LoggerFactory
+
+internal class ArbeidsgiveropplysningerRiver(
+    rapidsConnection: RapidsConnection
+) : River.PacketListener {
+    private companion object {
+        private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
+        private val logg = LoggerFactory.getLogger(this::class.java)
+        private const val behov = "Arbeidsgiveropplysninger"
+    }
+
+    init {
+        River(rapidsConnection).apply {
+            validate { it.demandAll("@behov", listOf(behov)) }
+            validate { it.rejectKey("@løsning") }
+        }.register(this)
+    }
+
+    override fun onError(problems: MessageProblems, context: MessageContext) {
+        sikkerlogg.error("forstod ikke $behov:\n${problems.toExtendedReport()}")
+    }
+
+    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        logg.info("Mottok Arbeidsgiveropplysninger-behov fra spleis med data:\n{}", packet.toJson())
+    }
+}
---
+++ arbeidsgiver/src/test/kotlin/no/nav/helse/sparkel/arbeidsgiver/ArbeidsgiveropplysningerRiverTest.kt
@@ -0,0 +1,65 @@
+package no.nav.helse.sparkel.arbeidsgiver
+
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.read.ListAppender
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import java.util.UUID
+
+internal class ArbeidsgiveropplysningerRiverTest {
+    private val objectMapper = jacksonObjectMapper()
+    private val testRapid = TestRapid().also(::ArbeidsgiveropplysningerRiver)
+    private val logCollector = ListAppender<ILoggingEvent>()
+    private val sikkerlogCollector = ListAppender<ILoggingEvent>()
+
+    init {
+        (LoggerFactory.getLogger(ArbeidsgiveropplysningerRiver::class.java) as Logger).addAppender(logCollector)
+        logCollector.start()
+        (LoggerFactory.getLogger("tjenestekall") as Logger).addAppender(logCollector)
+        logCollector.start()
+    }
+
+    @BeforeEach
+    fun beforeEach() {
+        logCollector.list.clear()
+        sikkerlogCollector.list.clear()
+    }
+
+    private fun behovMelding(behovType: String, løsning: String? = null): String =
+        objectMapper.valueToTree<JsonNode>(
+            mapOf(
+                "@id" to UUID.randomUUID(),
+                "@behov" to listOf(behovType),
+                "@løsning" to løsning
+            )
+        ).toString()
+
+    @Test
+    fun `logger ved gyldig behov`() {
+        testRapid.sendTestMessage(behovMelding("Arbeidsgiveropplysninger"))
+        assertEquals(1, logCollector.list.size)
+        assertTrue(logCollector.list.any { it.message.contains("Mottok Arbeidsgiveropplysninger-behov fra spleis med data") })
+        assertEquals(0, sikkerlogCollector.list.size)
+    }
+
+    @Test
+    fun `ignorerer løste behov`() {
+        testRapid.sendTestMessage(behovMelding("Arbeidsgiveropplysninger", "Test Løsning"))
+        assertEquals(0, logCollector.list.size)
+        assertEquals(0, sikkerlogCollector.list.size)
+    }
+
+    @Test
+    fun `logger ikke ved ugyldig behov`() {
+        testRapid.sendTestMessage(behovMelding("Tullebehov"))
+        assertEquals(0, logCollector.list.size)
+        assertEquals(0, sikkerlogCollector.list.size)
+    }
+}
