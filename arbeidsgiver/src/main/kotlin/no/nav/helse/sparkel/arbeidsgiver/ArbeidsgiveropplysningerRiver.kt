5a3150cf1b786b7ab3fe132a3d20b1ab58a6e1de
Gustav Berggren <83211434+b162214@users.noreply.github.com>
2022-10-13 16:03:27 +0200

    Arbeidgiveropplysninger producer i sparkel-arbeidsgiver (#14)
    Produser til ny arbeidsgiveropplysninger topic
    
    
    Co-authored-by: Mikael Bjerga <mikael.bjerga@nav.no>
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- arbeidsgiver/src/main/kotlin/no/nav/helse/sparkel/arbeidsgiver/ArbeidsgiveropplysningerRiver.kt
+++ arbeidsgiver/src/main/kotlin/no/nav/helse/sparkel/arbeidsgiver/ArbeidsgiveropplysningerRiver.kt
@@ -1,24 +1,35 @@
 package no.nav.helse.sparkel.arbeidsgiver
 
+import com.fasterxml.jackson.databind.JsonNode
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import no.nav.helse.rapids_rivers.asLocalDate
+import no.nav.helse.rapids_rivers.asLocalDateTime
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.internals.RecordHeader
 import org.slf4j.LoggerFactory
 
 internal class ArbeidsgiveropplysningerRiver(
-    rapidsConnection: RapidsConnection
+    private val rapidsConnection: RapidsConnection,
+    private val arbeidsgiverProducer: KafkaProducer<String, ArbeidsgiveropplysningerDTO>
+
 ) : River.PacketListener {
     private companion object {
         val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
         val logg = LoggerFactory.getLogger(this::class.java)
         const val eventName = "trenger_opplysninger_fra_arbeidsgiver"
     }
-
     init {
         River(rapidsConnection).apply {
             validate { it.demandValue("@event_name", eventName) }
+            validate { it.require("@opprettet", JsonNode::asLocalDateTime) }
+            validate { it.require("fom", JsonNode::asLocalDate) }
+            validate { it.require("tom", JsonNode::asLocalDate) }
+            validate { it.requireKey("organisasjonsnummer", "fødselsnummer") }
         }.register(this)
     }
 
@@ -33,7 +44,33 @@ internal class ArbeidsgiveropplysningerRiver(
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        logg.info("Mottok trenger_opplysninger_fra_arbeidsgiver-event fra spleis:\n{}", loggVennligPacket(packet))
-        sikkerlogg.info("Mottok trenger_opplysninger_fra_arbeidsgiver-event fra spleis med data:\n{}", packet.toJson())
+        "Mottok trenger_opplysninger_fra_arbeidsgiver-event fra spleis".let {
+            logg.info("$it:\n{}", loggVennligPacket(packet))
+            sikkerlogg.info("$it med data:\n{}", packet.toJson())
+        }
+
+        val payload = ArbeidsgiveropplysningerDTO(packet)
+        arbeidsgiverProducer.send(
+            ProducerRecord(
+                "arbeidsgiveropplysninger",
+                null,
+                payload.fødselsnummer,
+                payload,
+                listOf(RecordHeader("type", payload.meldingstype))
+            )
+        ).get()
+
+        "Publiserte forespørsel om arbeidsgiveropplyninger til helsearbeidsgiver-bro-sykepenger".let {
+            logg.info(it)
+            sikkerlogg.info("$it med data :\n{}", payload)
+        }
+
+        rapidsConnection.publish(
+            JsonMessage.newMessage(
+                mapOf(
+                    "@event_name" to "publisert_forespørsel_om_arbeidsgiveropplyninger"
+                )
+            ).toJson()
+        )
     }
 }
--- arbeidsgiver/src/test/kotlin/no/nav/helse/sparkel/arbeidsgiver/ArbeidsgiveropplysningerRiverTest.kt
+++ arbeidsgiver/src/test/kotlin/no/nav/helse/sparkel/arbeidsgiver/ArbeidsgiveropplysningerRiverTest.kt
@@ -4,22 +4,35 @@ import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.read.ListAppender
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import java.util.UUID
+import io.mockk.mockk
+import io.mockk.verify
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.internals.RecordHeader
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.UUID
 
 internal class ArbeidsgiveropplysningerRiverTest {
     private val objectMapper = jacksonObjectMapper()
-    private val testRapid = TestRapid().also(::ArbeidsgiveropplysningerRiver)
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .registerModules(JavaTimeModule())
+    private val testRapid = TestRapid()
+    private val mockproducer: KafkaProducer<String, ArbeidsgiveropplysningerDTO> = mockk(relaxed = true)
     private val logCollector = ListAppender<ILoggingEvent>()
     private val sikkerlogCollector = ListAppender<ILoggingEvent>()
 
     init {
+        ArbeidsgiveropplysningerRiver(testRapid, mockproducer)
         (LoggerFactory.getLogger(ArbeidsgiveropplysningerRiver::class.java) as Logger).addAppender(logCollector)
         logCollector.start()
         (LoggerFactory.getLogger("tjenestekall") as Logger).addAppender(sikkerlogCollector)
@@ -37,16 +50,23 @@ internal class ArbeidsgiveropplysningerRiverTest {
             mapOf(
                 "@id" to UUID.randomUUID(),
                 "@event_name" to eventName,
+                "@opprettet" to LocalDateTime.MAX,
+                "fom" to LocalDate.MIN,
+                "tom" to LocalDate.MAX,
+                "organisasjonsnummer" to "4321",
+                "fødselsnummer" to "123"
             )
         ).toString()
 
     @Test
     fun `logger ved gyldig event`() {
         testRapid.sendTestMessage(eventMelding("trenger_opplysninger_fra_arbeidsgiver"))
-        assertEquals(1, logCollector.list.size)
+        assertEquals(2, logCollector.list.size)
         assertTrue(logCollector.list.any { it.message.contains("Mottok trenger_opplysninger_fra_arbeidsgiver-event fra spleis") })
-        assertEquals(1, sikkerlogCollector.list.size)
+        assertTrue(logCollector.list.any { it.message.contains("Publiserte forespørsel om arbeidsgiveropplyninger til helsearbeidsgiver-bro-sykepenger") })
+        assertEquals(2, sikkerlogCollector.list.size)
         assertTrue(sikkerlogCollector.list.any { it.message.contains("Mottok trenger_opplysninger_fra_arbeidsgiver-event fra spleis med data") })
+        assertTrue(sikkerlogCollector.list.any { it.message.contains("Publiserte forespørsel om arbeidsgiveropplyninger til helsearbeidsgiver-bro-sykepenger") })
     }
 
     @Test
@@ -54,5 +74,31 @@ internal class ArbeidsgiveropplysningerRiverTest {
         testRapid.sendTestMessage(eventMelding("Tullebehov"))
         assertEquals(0, logCollector.list.size)
         assertEquals(0, sikkerlogCollector.list.size)
+    }
+
+    @Test
+    fun `publiserer forespørsel om arbeidsgiveropplysninger`() {
+        testRapid.sendTestMessage(eventMelding("trenger_opplysninger_fra_arbeidsgiver"))
+
+        val payload = ArbeidsgiveropplysningerDTO(
+            type = Meldingstype.TRENGER_OPPLYSNINGER_FRA_ARBEIDSGIVER,
+            organisasjonsnummer = "4321",
+            fødselsnummer = "123",
+            fom = LocalDate.MIN,
+            tom = LocalDate.MAX,
+            opprettet = LocalDateTime.MAX
+        )
+        verify(exactly = 1) {
+            mockproducer.send(
+                ProducerRecord(
+                    "arbeidsgiveropplysninger",
+                    null,
+                    payload.fødselsnummer,
+                    payload,
+                    listOf(RecordHeader("type", payload.meldingstype))
+                )
+            )
+        }
+        assertEquals(1, testRapid.inspektør.size)
     }
 }
