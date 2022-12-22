d91efc5327c06873148b9499c88119aa506d4977
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-12-22 17:02:45 +0100

    Source dependency til rapids-and-rivers, fordi jitpack svikter oss
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,8 @@ rootDir
     ?.filter { it.isDirectory && File(it, "build.gradle.kts").exists() }
     ?.forEach { include(it.name) }
 
+sourceControl {
+    gitRepository(java.net.URI("https://github.com/navikt/rapids-and-rivers.git")) {
+        producesModule("com.github.navikt:rapids-and-rivers")
+    }
+}
