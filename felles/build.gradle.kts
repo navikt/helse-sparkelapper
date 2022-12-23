8cbcdcd8a53cc27a0704b8143695ca0cf2b7c06f
Hege Haavaldsen <hege.haavaldsen@nav.no>
2022-12-23 09:53:24 +0100

    Tester å hardkode rapids and rivers versjon i fellesmodulen 🧪
    Co-authored-by: Helene Arnesen <helene.arnesen@nav.no>

--- felles/build.gradle.kts
+++ felles/build.gradle.kts
@@ -1,5 +1,5 @@
 val rapidsAndRiversVersion: String by project
 
 dependencies {
-    api("com.github.navikt:rapids-and-rivers:$rapidsAndRiversVersion")
+    api("com.github.navikt:rapids-and-rivers:2022122217191671725962.4c6c2077db70")
 }
