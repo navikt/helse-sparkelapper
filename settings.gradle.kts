466510a599d52f3fe2abed0035650e553cacffe5
Hege Haavaldsen <hege.haavaldsen@nav.no>
2022-12-23 13:22:06 +0100

    Fjermer dette, henter rapids og river på en annen måte
    Co-authored-by: Mikael Bjerga <mikael.bjerga@nav.no>
    Co-authored-by: Helene Arnesen <helene.arnesen@nav.no>
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,9 +3,3 @@ rootDir
     .listFiles()
     ?.filter { it.isDirectory && File(it, "build.gradle.kts").exists() }
     ?.forEach { include(it.name) }
-
-sourceControl {
-    gitRepository(java.net.URI("https://github.com/navikt/rapids-and-rivers.git")) {
-        producesModule("com.github.navikt:rapids-and-rivers")
-    }
-}
