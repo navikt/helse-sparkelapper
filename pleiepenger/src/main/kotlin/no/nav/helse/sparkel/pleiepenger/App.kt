4f342e4f80714a8d4c7da33d748a4570ff7c1078
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-02-14 16:31:37 +0100

    Jeg vil bare få azure credentials for GCP jeg 🤷‍

--- pleiepenger/src/main/kotlin/no/nav/helse/sparkel/pleiepenger/App.kt
+++ pleiepenger/src/main/kotlin/no/nav/helse/sparkel/pleiepenger/App.kt
@@ -1,5 +1,16 @@
 package no.nav.helse.sparkel.pleiepenger
 
+import io.ktor.http.ContentType
+import io.ktor.server.application.call
+import io.ktor.server.cio.CIO
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.response.respondText
+import io.ktor.server.response.respondTextWriter
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import io.prometheus.client.CollectorRegistry
+import io.prometheus.client.Counter
+import io.prometheus.client.exporter.common.TextFormat
 import java.net.URL
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -11,6 +22,30 @@ import no.nav.helse.sparkel.pleiepenger.k9.AbakusClient
 import no.nav.helse.sparkel.pleiepenger.k9.ClientSecretBasic
 
 fun main() {
+    if (gcp()) simpleApp()
+    else rapidsApp()
+}
+private fun gcp() = System.getenv("NAIS_CLUSTER_NAME")?.lowercase()?.endsWith("-gcp") == true
+private fun simpleApp() {
+    val requests = Counter.build("requests", "requests").labelNames("endpoint").register()
+    embeddedServer(CIO, port = 8080) {
+        routing {
+            get("/isalive") {
+                requests.labels("isalive").inc()
+                call.respondText("ALIVE!") }
+            get("/isready") {
+                requests.labels("isready").inc()
+                call.respondText("READY!") }
+            get("/metrics") {
+                requests.labels("metrics").inc()
+                call.respondTextWriter(ContentType.parse(TextFormat.CONTENT_TYPE_004)) {
+                    TextFormat.write004(this, CollectorRegistry.defaultRegistry.metricFamilySamples())
+                }
+            }
+        }
+    }.start(wait = true)
+}
+private fun rapidsApp() {
     val app = createApp(System.getenv())
     app.start()
 }
