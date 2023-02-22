31c64c2ca184538a5fbd2a27fd82cd9d36592681
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-02-22 14:53:23 +0100

    Sputnik deployes kun til GCP 🇷🇺
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>

--- sputnik/src/main/kotlin/no/nav/helse/sparkel/sputnik/App.kt
+++ sputnik/src/main/kotlin/no/nav/helse/sparkel/sputnik/App.kt
@@ -1,9 +1,22 @@
 package no.nav.helse.sparkel.sputnik
 
+import java.net.URL
 import no.nav.helse.rapids_rivers.RapidApplication
+import no.nav.helse.sparkel.abakus.AbakusClient
+import no.nav.helse.sparkel.abakus.ClientSecretPost
 
 fun main() {
-    clusterAvhengigStart()
+    val env = System.getenv()
+    val abakusClient = AbakusClient(
+        url = URL(env.getValue("ABAKUS_URL")),
+        accessTokenClient = ClientSecretPost(
+            tokenEndpoint = env.getValue("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"),
+            clientId = env.getValue("AZURE_APP_CLIENT_ID"),
+            clientSecret = env.getValue("AZURE_APP_CLIENT_SECRET"),
+            scope = env.getValue("ABAKUS_SCOPE")
+        )
+    )
+    startRapidsApplication(Abakusløser(abakusClient))
 }
 
 internal fun startRapidsApplication(foreldrepengerløser: Foreldrepengerløser) =
