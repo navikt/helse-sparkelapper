da254934147c0af80017be763a3140188000583c
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-02-23 16:22:18 +0100

    Tester forskjellige utfall av Foreldrepenger-behovet

--- sputnik/src/test/kotlin/no/nav/helse/sparkel/sputnik/SputnikTest.kt
+++ sputnik/src/test/kotlin/no/nav/helse/sparkel/sputnik/SputnikTest.kt
@@ -3,7 +3,6 @@ package no.nav.helse.sparkel.sputnik
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
-import java.util.UUID
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import no.nav.helse.sparkel.sputnik.abakus.AbakusClient
 import no.nav.helse.sparkel.sputnik.abakus.Stønadsperiode
@@ -25,8 +24,8 @@ internal class SputnikTest {
     }
 
     @Test
-    fun `en test`() {
-        testRapid.sendTestMessage(behov())
+    fun `Behov for Foreldrepenger - Har foreldrepenger, men ikke svangerskapspenger`() {
+        testRapid.sendTestMessage(foreldrepengerBehov(AltUntattSvangerskapspengerFødselsnummer))
         @Language("JSON")
         val forventet = """
         {
@@ -47,35 +46,117 @@ internal class SputnikTest {
         assertJsonEquals(forventet, testRapid.løsning())
     }
 
+    @Test
+    fun `Behov for Foreldrepenger - Har ikke foreldrepenger, men har svangerskapspenger`() {
+        testRapid.sendTestMessage(foreldrepengerBehov(AltUntattForeldrepengerFødselsnummer))
+        @Language("JSON")
+        val forventet = """
+        {
+            "Foreldrepenger": {
+                "Foreldrepengeytelse": null,
+                "Svangerskapsytelse": {
+                    "fom": "2018-01-01",
+                    "tom": "2018-01-31",
+                    "vedtatt": "2023-02-16T09:52:35.255",
+                    "perioder": [{
+                        "fom": "2018-01-01",
+                        "tom": "2018-01-31"
+                    }]
+                }
+            }
+        }
+        """
+        assertJsonEquals(forventet, testRapid.løsning())
+    }
+
+    @Test
+    fun `Behov for Foreldrepenger - Har foreldrepenger og svangerskapspenger`() {
+        testRapid.sendTestMessage(foreldrepengerBehov())
+        @Language("JSON")
+        val forventet = """
+        {
+            "Foreldrepenger": {
+                "Foreldrepengeytelse": {
+                    "fom": "2018-01-01",
+                    "tom": "2018-01-31",
+                    "vedtatt": "2023-02-16T09:52:35.255",
+                    "perioder": [{
+                        "fom": "2018-01-01",
+                        "tom": "2018-01-31"
+                    }]
+                },
+                "Svangerskapsytelse": {
+                    "fom": "2018-01-01",
+                    "tom": "2018-01-31",
+                    "vedtatt": "2023-02-16T09:52:35.255",
+                    "perioder": [{
+                        "fom": "2018-01-01",
+                        "tom": "2018-01-31"
+                    }]
+                }
+            }
+        }
+        """
+        assertJsonEquals(forventet, testRapid.løsning())
+    }
+
+    @Test
+    fun `Behov for Foreldrepenger - Har hverken foreldrepenger eller svangerskapspenger`() {
+        testRapid.sendTestMessage(foreldrepengerBehov(IngenYtelserFødselsnummer))
+        @Language("JSON")
+        val forventet = """
+        {
+            "Foreldrepenger": {
+                "Foreldrepengeytelse": null,
+                "Svangerskapsytelse": null
+            }
+        }
+        """
+        assertJsonEquals(forventet, testRapid.løsning())
+    }
+
     private fun TestRapid.løsning() = inspektør.message(0).path("@løsning").toString()
 
     private companion object {
         private val vedtatt = LocalDateTime.parse("2023-02-16T09:52:35.255").truncatedTo(ChronoUnit.MILLIS)
         @Language("JSON")
-        private fun behov() = """
+        private fun foreldrepengerBehov(fødselsnummer: String = "fødselsnummer") = """
         {
-            "@id": "${UUID.randomUUID()}",
             "@event_name":"behov",
             "@behov": ["Foreldrepenger"],
             "Foreldrepenger": {
                 "foreldrepengerFom": "2018-01-01",
                 "foreldrepengerTom": "2018-01-31"
             },
-            "fødselsnummer": "fødselsnummer"
+            "fødselsnummer": "$fødselsnummer"
         }
         """
 
         private fun assertJsonEquals(forventet: String, faktisk: String) = JSONAssert.assertEquals(forventet, faktisk, true)
 
+
+        private const val IngenYtelserFødselsnummer = "0"
+        private const val AltUntattSvangerskapspengerFødselsnummer = "1"
+        private const val AltUntattForeldrepengerFødselsnummer = "2"
+
         private val testAbakusClient = object : AbakusClient {
-            override fun hent(
-                fødselsnummer: String,
-                fom: LocalDate,
-                tom: LocalDate,
-                vararg ytelser: Ytelse
-            ): Set<Stønadsperiode> = setOf(
-                Stønadsperiode(fom, tom, 100, Ytelse("FORELDREPENGER"), vedtatt)
-            )
+            private val AlleYtelser = setOf("FORELDREPENGER", "SVANGERSKAPSPENGER", "PLEIEPENGER", "OMSORGSPENGER", "OPPLÆRINGSPENGER")
+            override fun hent(fødselsnummer: String, fom: LocalDate, tom: LocalDate, vararg ytelser: Ytelse): Set<Stønadsperiode> {
+               val medILøsning = when (fødselsnummer) {
+                   IngenYtelserFødselsnummer -> emptySet()
+                   AltUntattSvangerskapspengerFødselsnummer -> AlleYtelser.minus("SVANGERSKAPSPENGER")
+                   AltUntattForeldrepengerFødselsnummer -> AlleYtelser.minus("FORELDREPENGER")
+                   else -> AlleYtelser
+               }
+
+                val (snuteFom, snuteTom) = fom.minusMonths(1) to fom.minusDays(1)
+                val (haleFom, haleTom) = tom.plusDays(1) to tom.plusMonths(1)
+                return medILøsning.map { Ytelse(it) }.flatMap { listOf(
+                    Stønadsperiode(snuteFom, snuteTom, 100, it, vedtatt),
+                    Stønadsperiode(fom, tom, 100, it, vedtatt),
+                    Stønadsperiode(haleFom, haleTom, 100, it, vedtatt),
+                )}.toSet()
+            }
         }
     }
 
