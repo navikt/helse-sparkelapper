414363955303a55c483a5f387d515b1eb9604f8b
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-02-24 13:25:25 +0100

    Sletter pleiepenger-appen 🚮

--- pleiepenger/src/main/kotlin/no/nav/helse/sparkel/pleiepenger/SyktBarnKilde.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.helse.sparkel.pleiepenger
-
-import java.time.LocalDate
-
-internal interface SyktBarnKilde {
-    fun pleiepenger(fnr: String, fom: LocalDate, tom: LocalDate): Set<Stønadsperiode>
-    fun omsorgspenger(fnr: String, fom: LocalDate, tom: LocalDate): Set<Stønadsperiode>
-    fun opplæringspenger(fnr: String, fom: LocalDate, tom: LocalDate): Set<Stønadsperiode>
-}
--- pleiepenger/src/main/kotlin/no/nav/helse/sparkel/pleiepenger/infotrygd/Stønadstype.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.helse.sparkel.pleiepenger.infotrygd
-
-internal enum class Stønadstype(val url: String) {
-    PLEIEPENGER("/vedtak/pleiepenger"),
-    OMSORGSPENGER("/vedtak/omsorgspenger"),
-    OPPLAERINGSPENGER("/vedtak/opplaeringspenger")
-}
