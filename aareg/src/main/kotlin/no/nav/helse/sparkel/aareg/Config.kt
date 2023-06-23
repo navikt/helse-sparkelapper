ab94668094b613f43155879eb3d53aaf5675487d
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-06-23 17:48:21 +0200

    ♻️ Hjelpefunksjon

--- aareg/src/main/kotlin/no/nav/helse/sparkel/aareg/Config.kt
+++ aareg/src/main/kotlin/no/nav/helse/sparkel/aareg/Config.kt
@@ -5,22 +5,17 @@ import java.net.URL
 fun setUpEnvironment() =
     Environment(
         raw = System.getenv(),
-        organisasjonBaseUrl = System.getenv("ORGANISASJON_BASE_URL")
-            ?: error("Mangler env var ORGANISASJON_BASE_URL"),
-        kodeverkBaseUrl = System.getenv("KODEVERK_BASE_URL")
-            ?: error("Mangler env var KODEVERK_BASE_URL"),
-        aaregBaseUrlRest = System.getenv("AAREG_BASE_URL_REST")
-            ?: error("Mangler env var AAREG_BASE_URL_REST"),
-        aaregOauthScope = System.getenv("AAREG_OAUTH_SCOPE")
-            ?: error("Mangler env var AAREG_OAUTH_SCOPE"),
-        tokenEndpointURL = System.getenv("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT")?.let { URL(it) }
-            ?: error("Mangler env var AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"),
-        clientId = System.getenv("AZURE_APP_CLIENT_ID")
-            ?: error("Mangler env var AZURE_APP_CLIENT_ID"),
-        clientSecret = System.getenv("AZURE_APP_CLIENT_SECRET")
-            ?: error("Mangler env var AZURE_APP_CLIENT_SECRET"),
+        organisasjonBaseUrl = fromEnv("ORGANISASJON_BASE_URL"),
+        kodeverkBaseUrl = fromEnv("KODEVERK_BASE_URL"),
+        aaregBaseUrlRest = fromEnv("AAREG_BASE_URL_REST"),
+        aaregOauthScope = fromEnv("AAREG_OAUTH_SCOPE"),
+        tokenEndpointURL = URL(fromEnv("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT")),
+        clientId = fromEnv("AZURE_APP_CLIENT_ID"),
+        clientSecret = fromEnv("AZURE_APP_CLIENT_SECRET"),
         appName = System.getenv("NAIS_APP_NAME") ?: "sparkel-aareg"
     )
+
+private fun fromEnv(name: String) = System.getenv(name) ?: error("Mangler env var $name")
 
 data class Environment(
     val raw: Map<String, String>,
