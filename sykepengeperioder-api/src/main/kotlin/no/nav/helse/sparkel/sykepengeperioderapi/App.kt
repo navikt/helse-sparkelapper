256c1a01b07c96eb73d25c22671ccd9887386f4e
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-11-14 15:07:03 +0100

    Legger til endepunkt med autentisering 🫡
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- sykepengeperioder-api/src/main/kotlin/no/nav/helse/sparkel/sykepengeperioderapi/App.kt
+++ sykepengeperioder-api/src/main/kotlin/no/nav/helse/sparkel/sykepengeperioderapi/App.kt
@@ -1,21 +1,45 @@
 package no.nav.helse.sparkel.sykepengeperioderapi
 
+import com.auth0.jwk.JwkProviderBuilder
+import io.ktor.http.ContentType.Application.Json
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.auth.authenticate
+import io.ktor.server.auth.authentication
+import io.ktor.server.auth.jwt.JWTPrincipal
+import io.ktor.server.auth.jwt.jwt
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
-import java.time.LocalDateTime
+import java.net.URL
+
+private val String.env get() = checkNotNull(System.getenv(this)) { "Fant ikke environment variable $this" }
 
 fun main() {
     embeddedServer(CIO, port = 8080, module = Application::sykepengeperioderApi).start(wait = true)
 }
 
 private fun Application.sykepengeperioderApi() {
+    authentication {
+        jwt {
+            val jwkProvider = JwkProviderBuilder(URL("AZURE_OPENID_CONFIG_JWKS_URI".env)).build()
+            verifier(jwkProvider, "AZURE_OPENID_CONFIG_ISSUER".env) {
+                withAudience("AZURE_APP_CLIENT_ID".env)
+            }
+            validate { credentials -> JWTPrincipal(credentials.payload) }
+        }
+    }
+
     routing {
-        get("/isalive") { call.respondText("ÆØÅæøå ${LocalDateTime.now()}") }
-        get("/isready") { call.respondText("READY4U!") }
+        get("/isalive") { call.respondText("ISALIVE") }
+        get("/isready") { call.respondText("READY") }
+        authenticate {
+            post {
+                call.respondText("""{"sykepengeperioder":[]}""", Json)
+            }
+        }
     }
 }
