c2b943f9b2fdfc239d1213a617ddc93c016b3512
david steinsland <david@davidsteinsland.net>
2024-01-10 09:59:10 +0100

    tar bort unødvendige repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,15 +87,24 @@ allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     dependencies {
-        testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-        testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+        testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
 
     repositories {
-        maven("https://packages.confluent.io/maven/")
-        maven("https://oss.sonatype.org")
+        val githubPassword: String? by project
         mavenCentral()
+        /* ihht. https://github.com/navikt/utvikling/blob/main/docs/teknisk/Konsumere%20biblioteker%20fra%20Github%20Package%20Registry.md
+            så plasseres github-maven-repo (med autentisering) før nav-mirror slik at github actions kan anvende førstnevnte.
+            Det er fordi nav-mirroret kjører i Google Cloud og da ville man ellers fått unødvendige utgifter til datatrafikk mellom Google Cloud og GitHub
+         */
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/maven-release")
+            credentials {
+                username = "x-access-token"
+                password = githubPassword
+            }
+        }
         maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
     }
 
