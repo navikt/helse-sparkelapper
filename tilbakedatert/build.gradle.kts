48e4fe860178544ff1453847b1b84e694a44ff7f
Jonas Hasle <hei@jonashasle.no>
2024-01-22 14:56:16 +0100

    🎉 Publiserer sparkel-tilbakedatert i dev
    Co-authored-by: Øydis Kind Refsum <oydis.kind.refsum@nav.no>

---
+++ tilbakedatert/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("io.ktor:ktor-serialization-jackson:2.2.3")
+    implementation("io.ktor:ktor-client-logging:2.2.3")
+    implementation(project(":felles"))
+}
---
+++ tilbakedatert/src/main/kotlin/no.nav.helse.sparkel.tilbakedatert/TilbakedatertRiver.kt
@@ -0,0 +1,61 @@
+package no.nav.helse.sparkel.tilbakedatert
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.MessageProblems
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.River
+import org.slf4j.LoggerFactory
+
+internal class TilbakedatertRiver(
+    rapidsConnection: RapidsConnection,
+) : River.PacketListener {
+
+    companion object {
+        private val objectMapper = ObjectMapper()
+        private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
+        private val log = LoggerFactory.getLogger(TilbakedatertRiver::class.java)
+    }
+
+    init {
+        River(rapidsConnection).apply {
+            validate { it.demandKey("sykmelding") }
+        }.register(this)
+    }
+
+    override fun onError(problems: MessageProblems, context: MessageContext) {
+        sikkerlogg.error("forstod ikke sykmelding:\n${problems.toExtendedReport()}")
+    }
+
+    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        log.info("Leser melding ${packet["sykmelding"]["id"]}")
+        håndter(packet, context)
+    }
+
+    private fun håndter(packet: JsonMessage, context: MessageContext) {
+        sikkerlogg.info("Leser melding $packet")
+        return;
+//        val fnr = packet["fødselsnummer"].asText()
+//        val id = packet["@id"].asText()
+//        val sykmeldingId = packet["sykmeldingId"].asText()
+//        val erTilbakedatertSøknadHåndtert = false;
+//
+//        if (erTilbakedatertSøknadHåndtert) {
+//            val returEvent = objectMapper.createObjectNode()
+//                .put("@event_name", "tilbakedatering_behandlet")
+//                .put("fødselsnummer", fnr)
+//                .put("godkjent", true)
+//                .put("sykmeldingId", sykmeldingId)
+//
+//                context.publish(returEvent.toString()).also {
+//                    sikkerlogg.info(
+//                        "sender {} som {}",
+//                        StructuredArguments.keyValue("id", id),
+//                        packet.toJson()
+//                    )
+//                }
+//        }
+
+    }
+}
