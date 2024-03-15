a547d8af84bd4eecf528c44bae908089da410853
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-03-15 13:50:13 +0100

    👷 Logg output for oppgave-endret
    Så vi kanskje se hva som går gæli på GitHub?
    
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>

--- oppgave-endret/build.gradle.kts
+++ oppgave-endret/build.gradle.kts
@@ -1,3 +1,11 @@
 dependencies {
     implementation(project(":felles"))
+    tasks {
+        test {
+            testLogging {
+                events("failed", "standardOut", "standardError")
+                info.events("STARTED", "PASSED", "SKIPPED", "FAILED", "STANDARD_OUT", "STANDARD_ERROR")
+            }
+        }
+    }
 }
