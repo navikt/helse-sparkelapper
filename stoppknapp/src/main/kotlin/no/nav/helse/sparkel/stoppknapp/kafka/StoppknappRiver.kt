fcc54c79006e24410387a66dc7033f254d0ca32e
Elias Andreassen Thøgersen <elias3004@msn.com>
2024-04-30 10:10:02 +0200

    ✏️ orginal -> original

--- stoppknapp/src/main/kotlin/no/nav/helse/sparkel/stoppknapp/kafka/StoppknappRiver.kt
+++ stoppknapp/src/main/kotlin/no/nav/helse/sparkel/stoppknapp/kafka/StoppknappRiver.kt
@@ -1,8 +1,5 @@
 package no.nav.helse.sparkel.stoppknapp.kafka
 
-import java.time.LocalDateTime
-import java.time.OffsetDateTime
-import java.time.ZoneId
 import net.logstash.logback.argument.StructuredArguments.kv
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
@@ -10,6 +7,9 @@ import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.time.ZoneId
 
 internal class StoppknappRiver(rapidsConnection: RapidsConnection) :
     River.PacketListener {
@@ -42,31 +42,37 @@ internal class StoppknappRiver(rapidsConnection: RapidsConnection) :
         håndter(packet, context)
     }
 
-    private fun håndter(packet: JsonMessage, context: MessageContext) {
+    private fun håndter(
+        packet: JsonMessage,
+        context: MessageContext,
+    ) {
         val fødselsnummer: String = packet["sykmeldtFnr"]["value"].asText()
         val status: String = packet["status"].asText()
         val årsaker: List<String> = packet["arsakList"].map { it["type"].asText() }
         val tidsstempel: LocalDateTime = utcToLocalDateTime(packet["opprettet"].asText())
         val originalMelding: String = packet.toJson()
 
-        val returEvent = JsonMessage.newMessage(
-            eventName = "stans_automatisk_behandling",
-            map = mapOf(
-                "fødselsnummer" to fødselsnummer,
-                "status" to status,
-                "årsaker" to årsaker,
-                "tidsstempel" to tidsstempel,
-                "orginalMelding" to originalMelding
+        val returEvent =
+            JsonMessage.newMessage(
+                eventName = "stans_automatisk_behandling",
+                map =
+                    mapOf(
+                        "fødselsnummer" to fødselsnummer,
+                        "status" to status,
+                        "årsaker" to årsaker,
+                        "tidsstempel" to tidsstempel,
+                        "originalMelding" to originalMelding,
+                    ),
             )
-        )
 
         context.publish(returEvent.toJson()).also {
             sikkerlogg.info(
                 "sender stans_automatisk_behandling: {}",
-                kv("stans_automatisk_behandling", returEvent)
+                kv("stans_automatisk_behandling", returEvent),
             )
         }
     }
+
     private fun utcToLocalDateTime(dateTimeString: String): LocalDateTime =
         OffsetDateTime.parse(dateTimeString).atZoneSameInstant(ZoneId.of("Europe/Oslo")).toLocalDateTime()
 }
