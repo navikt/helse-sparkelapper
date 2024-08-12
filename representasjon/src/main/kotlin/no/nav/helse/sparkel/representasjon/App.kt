5c54a5695f9a0495fa8e404255d81d9279c14a10
Øyvind Hagen <oyvind.hagen@nav.no>
2024-08-12 15:22:40 +0200

    🚧 Setter timeouts på httpklient
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- representasjon/src/main/kotlin/no/nav/helse/sparkel/representasjon/App.kt
+++ representasjon/src/main/kotlin/no/nav/helse/sparkel/representasjon/App.kt
@@ -3,8 +3,10 @@ package no.nav.helse.sparkel.representasjon
 import com.github.navikt.tbd_libs.azure.createAzureTokenClientFromEnvironment
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.jackson.jackson
+import java.time.Duration
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 
@@ -17,6 +19,11 @@ internal fun createApp(env: Map<String, String>): RapidsConnection {
     return RapidApplication.create(env).apply {
         val httpClient = HttpClient(Apache) {
             install(ContentNegotiation) { jackson() }
+            install(HttpTimeout) {
+                connectTimeoutMillis = Duration.ofSeconds(60).toMillis()
+                requestTimeoutMillis = Duration.ofSeconds(60).toMillis()
+                socketTimeoutMillis = Duration.ofSeconds(60).toMillis()
+            }
         }
 
         val azureClient = createAzureTokenClientFromEnvironment(env)
