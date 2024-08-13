8b929789af3ab1e05a076d5b5b8f18e3aab17df1
Martin Remøy Solheim <martin.solheim@me.com>
2024-08-13 14:30:01 +0200

    🚧 Mapper om data som sendes som svar på Fullmakt behov
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- representasjon/src/main/kotlin/no/nav/helse/sparkel/representasjon/RepresentasjonRiver.kt
+++ representasjon/src/main/kotlin/no/nav/helse/sparkel/representasjon/RepresentasjonRiver.kt
@@ -1,11 +1,14 @@
 package no.nav.helse.sparkel.representasjon
 
+import java.time.LocalDate
 import net.logstash.logback.argument.StructuredArguments.kv
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import no.nav.helse.rapids_rivers.asLocalDate
+import no.nav.helse.sparkel.representasjon.RepresentasjonRiver.Område.*
 import org.slf4j.LoggerFactory
 
 internal class RepresentasjonRiver(
@@ -21,7 +24,7 @@ internal class RepresentasjonRiver(
     init {
         River(rapidsConnection).apply {
             validate {
-                it.demandValue("@event_name", "fullmakt")
+                it.demandAll("@behov", listOf("Fullmakt"))
                 it.rejectKey("@løsning")
                 it.requireKey("@id", "fødselsnummer")
             }
@@ -38,6 +41,14 @@ internal class RepresentasjonRiver(
         val fnr = packet["fødselsnummer"].asText()
 
         val svar = representasjonClient.hentFullmakt(fnr)
+            ?.map {
+                Fullmakt(
+                    områder = it["omraade"].map { område -> Område.fra(område["tema"].asText()) },
+                    gyldigFraOgMed = it["gyldigFraOgMed"].asLocalDate(),
+                    gyldigTilOgMed = it["gyldigTilOgMed"].asLocalDate()
+                )
+            }
+            ?.filter { it.områder.any { område -> område in listOf(Syk, Sym, Alle) } }
         packet["@løsning"] = mapOf("fullmakt" to (svar ?: mapOf("oppslagFeilet" to true)))
         context.publish(packet.toJson())
         sikkerlogg.info(
@@ -46,5 +57,25 @@ internal class RepresentasjonRiver(
             packet.toJson()
         )
 
+    }
+
+    internal data class Fullmakt(
+        val områder: List<Område>,
+        val gyldigFraOgMed: LocalDate,
+        val gyldigTilOgMed: LocalDate
+    )
+    enum class Område {
+        Alle, Syk, Sym, Annet;
+
+        companion object {
+            fun fra(verdi: String): Område {
+                return when (verdi) {
+                    "*" -> Alle
+                    "SYK" -> Syk
+                    "SYM" -> Sym
+                    else -> Annet
+                }
+            }
+        }
     }
 }
