7f679e14a7e85baa86598ca187cf26ce14ecffbd
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-09-26 21:57:32 +0200

    ✅ Tester for corner cases for sykmelding-eventer
    De på team sykmelding anbefaler å tåle at `merknader` både kan være tom
    og mangle.

--- tilbakedatert/src/test/kotlin/no/nav/helse/sparkel/tilbakedatert/TilbakedatertRiverTest.kt
+++ tilbakedatert/src/test/kotlin/no/nav/helse/sparkel/tilbakedatert/TilbakedatertRiverTest.kt
@@ -4,38 +4,22 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.UUID
 import no.nav.helse.rapids_rivers.asLocalDate
 import org.intellij.lang.annotations.Language
-import org.junit.jupiter.api.TestInstance
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class TilbakedatertRiverTest {
 
-    private lateinit var rapid: TestRapid
+    private val rapid: TestRapid = TestRapid().apply(::TilbakedatertRiver)
 
-    @BeforeAll
-    fun setup() {
-        rapid = TestRapid().apply {
-            TilbakedatertRiver(this)
-        }
-    }
+    private fun sendEvent(behov: String) = rapid.sendTestMessage(behov)
 
-    @BeforeEach
-    fun clear() {
-        rapid.reset()
-    }
-
-    private fun testEvent(behov: String) {
-        TilbakedatertRiver(rapid)
-        rapid.sendTestMessage(behov)
-    }
+    private fun merknaderMedInnhold(type: String) = """ "merknader": [{"type": "$type"}] """
+    private fun merknaderErNull() = """ "merknader": null """
 
     @Language("JSON")
-    private fun enkeltEvent(fom: LocalDate? = LocalDate.now().minusDays(4), signaturDato: LocalDateTime? = LocalDateTime.now(), merknader: String? = null) =
+    private fun sykmeldingEvent(fom: LocalDate? = LocalDate.now().minusDays(4), signaturDato: LocalDateTime? = LocalDateTime.now(), merknader: String?) =
         """
         {
              "sykmelding": {
@@ -49,14 +33,14 @@ internal class TilbakedatertRiverTest {
                     }
                 ]
             },
-            "personNrPasient": "12345678910",
-            "merknader": $merknader
+            "personNrPasient": "12345678910" ${if (merknader != null) "${','}" else ""}
+            ${merknader ?: ""}
         }
         """
 
     @Test
     fun `Sender tilbakedatering_behandlet dersom sykmelding er tilbakedatert og godkjent`() {
-        testEvent(enkeltEvent())
+        sendEvent(sykmeldingEvent(merknader = merknaderErNull()))
 
         val svar = rapid.inspektør.message(0)
         assertEquals(svar["@event_name"].asText(), "tilbakedatering_behandlet")
@@ -65,32 +49,44 @@ internal class TilbakedatertRiverTest {
     }
 
     @Test
+    fun `Sender tilbakedatering_behandlet dersom sykmelding er tilbakedatert og merknader mangler i meldingen`() {
+        sendEvent(sykmeldingEvent(merknader = null))
+        assertEquals(rapid.inspektør.field(0, "@event_name").asText(), "tilbakedatering_behandlet")
+    }
+
+    @Test
+    fun `Sender tilbakedatering_behandlet dersom sykmelding er tilbakedatert og merknader er tom liste`() {
+        sendEvent(sykmeldingEvent(merknader = """ "merknader": [] """))
+        assertEquals(rapid.inspektør.field(0, "@event_name").asText(), "tilbakedatering_behandlet")
+    }
+
+    @Test
     fun `Sender ikke tilbakedatering_behandlet dersom sykmelding ikke er tilbakedatert`() {
-        testEvent(enkeltEvent(fom = LocalDate.now(), signaturDato = LocalDateTime.now()))
+        sendEvent(sykmeldingEvent(fom = LocalDate.now(), signaturDato = LocalDateTime.now(), merknader = merknaderErNull()))
         assertEquals(0, rapid.inspektør.size)
     }
 
     @Test
     fun `Sender ikke tilbakedatering_behandlet dersom sykmelding er tilbakedatert men er UNDER_BEHANDLING`() {
-        testEvent(enkeltEvent(merknader = "[{\"type\":\"UNDER_BEHANDLING\"}]"))
+        sendEvent(sykmeldingEvent(merknader = merknaderMedInnhold("UNDER_BEHANDLING")))
         assertEquals(0, rapid.inspektør.size)
     }
 
     @Test
     fun `Sender ikke tilbakedatering_behandlet dersom UGYLDIG_TILBAKEDATERING`() {
-        testEvent(enkeltEvent(merknader = "[{\"type\":\"UGYLDIG_TILBAKEDATERING\"}]"))
+        sendEvent(sykmeldingEvent(merknader = merknaderMedInnhold("UGYLDIG_TILBAKEDATERING")))
         assertEquals(0, rapid.inspektør.size)
     }
 
     @Test
     fun `Sender ikke tilbakedatering_behandlet dersom TILBAKEDATERING_KREVER_FLERE_OPPLYSNINGER`() {
-        testEvent(enkeltEvent(merknader = "[{\"type\":\"TILBAKEDATERING_KREVER_FLERE_OPPLYSNINGER\"}]"))
+        sendEvent(sykmeldingEvent(merknader = merknaderMedInnhold("TILBAKEDATERING_KREVER_FLERE_OPPLYSNINGER")))
         assertEquals(0, rapid.inspektør.size)
     }
 
     @Test
     fun `Sender ikke tilbakedatering_behandlet dersom DELVIS_GODKJENT`() {
-        testEvent(enkeltEvent(merknader = "[{\"type\":\"DELVIS_GODKJENT\"}]"))
+        sendEvent(sykmeldingEvent(merknader = merknaderMedInnhold("DELVIS_GODKJENT")))
         assertEquals(0, rapid.inspektør.size)
     }
 }
