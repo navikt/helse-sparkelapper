6d8fb75583221628e2024ea14ca0fda7fc3c9d2a
david steinsland <david@davidsteinsland.net>
2024-11-05 15:15:43 +0100

    trigger et bygg

--- gosys/build.gradle.kts
+++ gosys/build.gradle.kts
@@ -1,4 +1,5 @@
 val tbdLibsVersion: String by project
+
 dependencies {
     implementation("com.github.navikt.tbd-libs:azure-token-client-default:$tbdLibsVersion")
     implementation("com.github.navikt.tbd-libs:speed-client:$tbdLibsVersion")
