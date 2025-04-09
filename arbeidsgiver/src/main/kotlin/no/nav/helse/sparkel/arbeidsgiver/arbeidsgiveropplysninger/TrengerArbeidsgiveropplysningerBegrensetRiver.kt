19010f0bd67c40e986f7839fa75cbf2e1c147edb
Hege Haavaldsen <hege.haavaldsen@nav.no>
2025-04-09 14:40:27 +0200

    Trenger ikke tilstanden lenger, da denne logikken er flyttet til utledingen av trengerArbeidsgiveropplysninger i spleis
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>

--- arbeidsgiver/src/main/kotlin/no/nav/helse/sparkel/arbeidsgiver/arbeidsgiveropplysninger/TrengerArbeidsgiveropplysningerBegrensetRiver.kt
+++ arbeidsgiver/src/main/kotlin/no/nav/helse/sparkel/arbeidsgiver/arbeidsgiveropplysninger/TrengerArbeidsgiveropplysningerBegrensetRiver.kt
@@ -24,7 +24,6 @@ internal class TrengerArbeidsgiveropplysningerBegrensetRiver(
         River(rapidsConnection).apply {
             precondition { it.requireValue("@event_name", eventName) }
             precondition { it.requireValue("trengerArbeidsgiveropplysninger", true) }
-            precondition { it.requireAny("tilstand", listOf("START", "AVVENTER_INFOTRYGDHISTORIKK")) }
             precondition { it.forbidValues("organisasjonsnummer", listOf("ARBEIDSLEDIG", "SELVSTENDIG", "FRILANS")) }
             validate {
                 it.requireKey(
