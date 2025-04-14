e9ea62a0037384ef0e33a9e69cbff156779f496c
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-04-14 09:51:55 +0200

    🚧 Ny TilbakedatertRiver har fått mer kjøtt på beinet sitt

--- tilbakedatert/src/main/kotlin/no.nav.helse.sparkel.tilbakedatert/NyTilbakedatertRiver.kt
+++ tilbakedatert/src/main/kotlin/no.nav.helse.sparkel.tilbakedatert/NyTilbakedatertRiver.kt
@@ -4,8 +4,10 @@ import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
 import com.github.navikt.tbd_libs.rapids_and_rivers.River
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageProblems
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import io.micrometer.core.instrument.MeterRegistry
+import net.logstash.logback.argument.StructuredArguments.keyValue
 
 internal class NyTilbakedatertRiver(
     rapidsConnection: RapidsConnection,
@@ -14,12 +16,50 @@ internal class NyTilbakedatertRiver(
     init {
         River(rapidsConnection).apply {
             precondition {
-                it.requireKey("metadata")
+                it.requireValue("validation.status", "OK")
+                it.requireKey("sykmelding.id")
+                it.requireKey("sykmelding.pasient.id")
+                it.require("validation.rules") { node ->
+                    check(node.isArray)
+                    check(node.any { rule ->
+                        rule["name"].asText() == "TILBAKEDATERING_UNDER_BEHANDLING" &&
+                            rule["type"].asText() == "OK"
+                    })
+                }
             }
         }.register(this)
     }
 
+    override fun onError(problems: MessageProblems, context: MessageContext, metadata: MessageMetadata) {
+        sikkerlogg.error(problems.toExtendedReport())
+    }
+
+    override fun onSevere(error: MessageProblems.MessageException, context: MessageContext) {
+        sikkerlogg.info(error.toString())
+    }
     override fun onPacket(packet: JsonMessage, context: MessageContext, metadata: MessageMetadata, meterRegistry: MeterRegistry) {
         sikkerlogg.info("Leser melding {}", packet.toJson())
+        val fødselsnummer = packet["sykmelding.pasient.id"].asText()
+        val sykmeldingId = packet["sykmelding.id"].asText()
+
+        val returEvent = lagReturEvent(fødselsnummer, sykmeldingId)
+        context.publish(fødselsnummer, returEvent).also {
+            sikkerlogg.info(
+                "sender tilbakedatering_behandlet for {}:\n{}",
+                keyValue("sykmeldingId", sykmeldingId),
+                returEvent
+            )
+        }
     }
+    private fun lagReturEvent(
+        fødselsnummer: String,
+        sykmeldingId: String
+    ) = JsonMessage.newMessage(
+        eventName = "tilbakedatering_behandlet",
+        map = mapOf(
+            "fødselsnummer" to fødselsnummer,
+            "sykmeldingId" to sykmeldingId,
+        )
+    ).toJson()
+
 }
---
+++ tilbakedatert/src/test/kotlin/no/nav/helse/sparkel/tilbakedatert/NyTilbakedatertRiverTest.kt
@@ -0,0 +1,115 @@
+package no.nav.helse.sparkel.tilbakedatert
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.navikt.tbd_libs.rapids_and_rivers.test_support.TestRapid
+import java.util.UUID
+import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class NyTilbakedatertRiverTest {
+
+    private val rapid: TestRapid = TestRapid().apply(::NyTilbakedatertRiver)
+
+    private fun sendEvent(behov: String) = rapid.sendTestMessage(behov)
+
+    @Test
+    fun `Sender ikke tilbakedatering_behandlet for OK sykmelding`() {
+        sendEvent(Meldinger.Ok)
+        assertEquals(0, rapid.inspektør.size)
+    }
+
+    @Test
+    fun `Sender ikke tilbakedatering_behandlet for sykmelding som er under behandling`() {
+        sendEvent(Meldinger.UnderBehandling)
+        assertEquals(0, rapid.inspektør.size)
+    }
+
+    @Test
+    fun `Sender tilbakedatering_behandlet for godkjente sykmeldinger`() {
+        sendEvent(Meldinger.Godkjent)
+        assertEquals(1, rapid.inspektør.size)
+        val expectedSykmeldingId = jacksonObjectMapper().readTree(Meldinger.Godkjent)["sykmelding"]["id"].asText()
+        assertEquals(expectedSykmeldingId, rapid.inspektør.field(0, "sykmeldingId").asText())
+    }
+
+    object Meldinger {
+        @Language("JSON")
+        internal val Ok = """
+            {
+                "sykmelding": {
+                    "id": "${UUID.randomUUID()}",
+                    "pasient": {
+                        "id": "424242"
+                    }
+                },
+                "validation": {
+                    "status": "OK",
+                    "timestamp": "2025-03-26T23:00:00Z",
+                    "rules": []
+                }
+            }
+        """.trimIndent()
+
+        @Language("JSON")
+        internal val UnderBehandling = """
+            {
+                "sykmelding": {
+                    "id": "${UUID.randomUUID()}",
+                    "pasient": {
+                        "id": "424242"
+                    }
+                },
+                "validation": {
+                    "status": "PENDING",
+                    "timestamp": "2025-04-04T14:15:15.597552892Z",
+                    "rules": [
+                        {
+                            "name": "TILBAKEDATERING_UNDER_BEHANDLING",
+                            "timestamp": "2025-04-04T14:15:15.597552892Z",
+                            "description": "Sykmeldingen er til manuell behandling",
+                            "validationType": "AUTOMATIC",
+                            "type": "PENDING"
+                        }
+                    ]
+                }
+            }
+        """.trimIndent()
+
+        @Language("JSON")
+        internal val Godkjent = """
+            {
+                "sykmelding": {
+                    "id": "${UUID.randomUUID()}",
+                    "pasient": {
+                        "id": "424242"
+                    }
+                },
+                "validation": {
+                    "status": "OK",
+                    "timestamp": "2025-04-04T14:52:34.308156427Z",
+                    "rules": [
+                        {
+                            "name": "TILBAKEDATERING_UNDER_BEHANDLING",
+                            "description": "Sykmeldingen er til manuell behandling",
+                            "timestamp": "2025-04-04T14:52:34.308156427Z",
+                            "validationType": "MANUAL",
+                            "type": "OK",
+                            "outcome": {
+                                "outcome": "OK",
+                                "timestamp": "2025-04-04T14:52:34.308156427Z"
+                            }
+                        },
+                        {
+                            "name": "TILBAKEDATERING_UNDER_BEHANDLING",
+                            "timestamp": "2025-04-04T14:15:15.597552892Z",
+                            "description": "Sykmeldingen er til manuell behandling",
+                            "validationType": "AUTOMATIC",
+                            "type": "PENDING"
+                        }
+                    ]
+                }
+            }
+        """.trimIndent()
+    }
+}
