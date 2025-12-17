0c567a2ab56421532e5a90b335c935c203a29281
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2025-12-17 12:31:01 +0100

    bumper junit, mest for å se om det trigger komplett nytt bygg, for jeg har så lyst på friske dockerimages

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("jvm") version "2.2.21"
 }
 
-val junitJupiterVersion = "5.12.1"
+val junitJupiterVersion = "6.0.1"
 val rapidsAndRiversVersion = "2025110410191762247980.5e0592e08597"
 val tbdLibsVersion = "2025.11.04-10.12-3a167132"
 val ktorVersion = "3.2.3"
