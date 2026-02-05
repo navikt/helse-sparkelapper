929f3b044fea3e99655f43128b04eed79b6bb234
Linus Johansson <linus.123@hotmail.com>
2026-02-05 12:21:32 +0100

    Drept arena 🔪🩸
    Co-authored-by: Sivert Lunsæter <sivert.frang.lunseter@nav.no>

--- arena/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-val tbdLibsVersion: String by project
-
-dependencies {
-    implementation("com.github.navikt.tbd-libs:azure-token-client-default:$tbdLibsVersion")
-    implementation("com.github.navikt.tbd-libs:minimal-soap-client:$tbdLibsVersion")
-    implementation(project(":felles"))
-
-    testImplementation("com.github.navikt.tbd-libs:mock-http-client:$tbdLibsVersion")
-}
